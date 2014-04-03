aa1764c70858fd1b2f1f53c1c289eaa9228b7c33
Simanta J Handique <shandique@thoughtworks.com>
2014-04-03 16:17:32 +0530

    Aravind,simij | #147 | tests.

--- src/test/js/app/bg/metadata.sync.spec.js
+++ src/test/js/app/bg/metadata.sync.spec.js
@@ -33,7 +33,7 @@ define(["metadataSync", "Q", "utils", "properties"], function(metadataSync, q, u
 
         });
 
-        it("should fetch all metadata from file the first time", function(done) {
+        it("should sync metadata and update changelog", function(done) {
             spyOn(httpWrapper, "get").and.returnValue(utils.getPromise(q, data));
             spyOn(idb, "get").and.returnValue(utils.getPromise(q, {
                 "lastUpdatedTime": today
@@ -45,6 +45,27 @@ define(["metadataSync", "Q", "utils", "properties"], function(metadataSync, q, u
 
             metadataSync.sync().then(function() {
                 expect(httpWrapper.get.calls.argsFor(0)[0]).toEqual(properties.metadata.url + '?lastUpdated=' + today);
+                expect(idb.put.calls.allArgs()).toEqual([
+                    ['categories', category1, undefined],
+                    ['changeLog', {
+                        type: 'metaData',
+                        lastUpdatedTime: tomorrow
+                    }]
+                ]);
+                done();
+            });
+        });
+
+        it("should pull all metadata if syncing for the first time", function(done) {
+            spyOn(httpWrapper, "get").and.returnValue(utils.getPromise(q, data));
+            spyOn(idb, "get").and.returnValue(utils.getPromise(q, undefined));
+            spyOn(idb, "usingTransaction").and.callFake(function(stores, fn) {
+                return fn();
+            });
+            spyOn(idb, "put").and.returnValue(utils.getPromise(q, {}));
+
+            metadataSync.sync().then(function() {
+                expect(httpWrapper.get.calls.argsFor(0)[0]).toEqual(properties.metadata.url);
                 expect(idb.put.calls.allArgs()).toEqual([
                     ['categories', category1, undefined],
                     ['changeLog', {
