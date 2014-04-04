73cdb79d4b996eadef207afcddfab1ee661e2e20
Aravind M <arav.psgtech@gmail.com>
2014-04-04 11:57:37 +0530

    Aravind,Simij | #147 | Scoping registerCallback.

--- src/main/js/app/bg/metadata.sync.js
+++ src/main/js/app/bg/metadata.sync.js
@@ -53,12 +53,14 @@ msf.metadata = function() {
     };
 }();
 
-var registerCallback = function(alarmName, callback) {
-    return function(alarm) {
-        if (alarm.name === alarmName)
-            callback();
+(function() {
+    var registerCallback = function(alarmName, callback) {
+        return function(alarm) {
+            if (alarm.name === alarmName)
+                callback();
+        };
     };
-};
 
-if (chrome.alarms)
-    chrome.alarms.onAlarm.addListener(registerCallback("metadataSyncAlarm", msf.metadata.sync));
+    if (chrome.alarms)
+        chrome.alarms.onAlarm.addListener(registerCallback("metadataSyncAlarm", msf.metadata.sync));
+})();
