4855598cdb0f6df6d9de5068f2ce8b71cfe48126
Aravind M <arav.psgtech@gmail.com>
2014-04-19 19:01:02 +0530

    Aravind | cleaning up scripts

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,36 +1,7 @@
 #!/bin/bash
-
-function do_curl 
-{
-	curl -o $destination -u $authorization $url || exit
-}
-
-function usage
-{
-	echo "usage: ./package.sh username:password url destinationFilePath"
-}
-
-function package_chrome_app
-{
+function package_chrome_app {
 	 google-chrome --pack-extension=src/main
 	 mv src/main.crx src/dhis2.crx
 }
 
-##### Main
-if [ "$1" == "" ]; then
-	usage
-	exit 1
-fi
-
-if [ "$2" == "" ]; then
-	usage
-	exit 1
-fi
-
-
-authorization=$1
-url=$2"/api/metadata.json"
-destination="src/main/data/metadata.json"
-
-do_curl
 package_chrome_app
