ca227a8d445c0feb023e5737a8b239e3ea9d3573
Aravind M <arav.psgtech@gmail.com>
2014-04-22 11:46:05 +0530

    Revert "Aravind | cleaning up scripts"
    This reverts commit a58064b031efd4c02c7fc5e26890fcf988fdc8ae.

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,7 +1,36 @@
 #!/bin/bash
-function package_chrome_app {
+
+function do_curl 
+{
+	curl -o $destination -u $authorization $url || exit
+}
+
+function usage
+{
+	echo "usage: ./package.sh username:password url destinationFilePath"
+}
+
+function package_chrome_app
+{
 	 google-chrome --pack-extension=src/main
 	 mv src/main.crx src/dhis2.crx
 }
 
+##### Main
+if [ "$1" == "" ]; then
+	usage
+	exit 1
+fi
+
+if [ "$2" == "" ]; then
+	usage
+	exit 1
+fi
+
+
+authorization=$1
+url=$2"/api/metadata.json"
+destination="src/main/data/metadata.json"
+
+do_curl
 package_chrome_app
