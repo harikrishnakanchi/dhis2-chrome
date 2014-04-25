e13fffb45a15a23d564013e67ace55737f7912c4
Mansi Singhal <msinghal@thoughtworks.com>
2014-04-25 10:33:23 +0530

    Mahendra,Mansi | #45, Adding test for delete module

--- src/test/unit/js/app/controller/module.controller.spec.js
+++ src/test/unit/js/app/controller/module.controller.spec.js
@@ -2,7 +2,7 @@
 define(["moduleController", "angularMocks", "utils"], function(ModuleController, mocks, utils) {
     describe("op unit controller", function() {
 
-        var scope, moduleController, projectsService, mockOrgStore, db, q, location, _Date;
+        var scope, moduleController, projectsService, mockOrgStore, db, q, location, _Date, datasets;
 
         beforeEach(mocks.inject(function($rootScope, $q, $location) {
             scope = $rootScope.$new();
@@ -29,14 +29,7 @@ define(["moduleController", "angularMocks", "utils"], function(ModuleController,
                 return today;
             };
 
-        }));
-
-        afterEach(function() {
-            Date = _Date;
-        });
-
-        it("should get all datasets", function() {
-            var datasets = [{
+            datasets = [{
                 name: "Malaria",
                 id: "dataset_1"
             }, {
@@ -48,11 +41,41 @@ define(["moduleController", "angularMocks", "utils"], function(ModuleController,
             spyOn(mockOrgStore, 'getAll').and.returnValue(utils.getPromise(q, datasets));
             moduleController = new ModuleController(scope, projectsService, db, location);
 
+        }));
+
+        afterEach(function() {
+            Date = _Date;
+        });
+
+        it("should get all datasets", function() {
             scope.$apply();
 
             expect(db.objectStore).toHaveBeenCalledWith("dataSets");
             expect(mockOrgStore.getAll).toHaveBeenCalled();
             expect(scope.dataSets).toEqual(datasets);
+        });
+
+        it('should delete module', function() {
+            scope.modules = [{
+                'name': 'Module1'
+            }, {
+                'name': 'Module2'
+            }, {
+                'name': 'Module1'
+            }, {
+                'name': 'Module4'
+            }];
+            scope.$apply();
+
+            scope.delete(2);
+            expect(scope.modules).toEqual([{
+                'name': 'Module1'
+            }, {
+                'name': 'Module2'
+            }, {
+                'name': 'Module4'
+            }]);
+
         });
     });
 });
