b0d5341d9b87442b86d521e94f0d56e95cb10c82
Mansi Singhal <msinghal@thoughtworks.com>
2014-05-06 18:46:36 +0530

    Aravind,Mansi | #45, setting spinner for only http request, ignoring template requests.

--- src/main/js/app/interceptors/http.interceptor.js
+++ src/main/js/app/interceptors/http.interceptor.js
@@ -1,27 +1,32 @@
 define(["properties"], function(properties) {
     return function($rootScope, $q) {
+        var finishRequest = function(response) {
+            if (response.config.url.indexOf('templates/') !== 0) {
+                $rootScope.pendingRequests -= 1;
+                if ($rootScope.pendingRequests === 0) {
+                    $rootScope.loading = false;
+                }
+            }
+        };
+
         $rootScope.pendingRequests = 0;
         return {
             'request': function(config) {
-                $rootScope.loading = true;
-                $rootScope.pendingRequests += 1;
+                if (config.url.indexOf('templates/') !== 0) {
+                    $rootScope.loading = true;
+                    $rootScope.pendingRequests += 1;
+                }
                 if (config.url.indexOf(properties.dhis.url) === 0) {
                     config.headers.Authorization = properties.dhis.auth_header;
                 }
                 return config;
             },
             'response': function(response) {
-                $rootScope.pendingRequests -= 1;
-                if ($rootScope.pendingRequests === 0) {
-                    $rootScope.loading = false;
-                }
+                finishRequest(response);
                 return response;
             },
             'responseError': function(rejection) {
-                $rootScope.pendingRequests -= 1;
-                if ($rootScope.pendingRequests === 0) {
-                    $rootScope.loading = false;
-                }
+                finishRequest(rejection);
                 return $q.reject(rejection);
             }
         };
--- src/test/unit/js/app/interceptors/http.interceptor.spec.js
+++ src/test/unit/js/app/interceptors/http.interceptor.spec.js
@@ -16,21 +16,51 @@ define(["httpInterceptor", "angularMocks", "properties"], function(HttpIntercept
             expect(rootScope.pendingRequests).toEqual(1);
         });
 
+        it("should not handle template requests", function() {
+            httpInterceptor.request({
+                'url': "templates/something"
+            });
+            expect(rootScope.loading).toEqual();
+            expect(rootScope.pendingRequests).toEqual(0);
+        });
+
         it("should set loading to false if there are no pending requests", function() {
             rootScope.pendingRequests = 1;
             rootScope.loading = true;
 
-            httpInterceptor.response({});
+            httpInterceptor.response({
+                "config": {
+                    "url": "templates123/blah"
+                }
+            });
 
             expect(rootScope.loading).toEqual(false);
             expect(rootScope.pendingRequests).toEqual(0);
+        });
+
+        it("should not handle template request responses", function() {
+            rootScope.pendingRequests = 1;
+            rootScope.loading = true;
+
+            httpInterceptor.response({
+                "config": {
+                    "url": "templates/blah"
+                }
+            });
+
+            expect(rootScope.loading).toEqual(true);
+            expect(rootScope.pendingRequests).toEqual(1);
         });
 
         it("should not change loading if there are pending requests", function() {
             rootScope.pendingRequests = 2;
             rootScope.loading = true;
 
-            httpInterceptor.response({});
+            httpInterceptor.response({
+                "config": {
+                    "url": "someurl"
+                }
+            });
 
             expect(rootScope.loading).toEqual(true);
             expect(rootScope.pendingRequests).toEqual(1);
@@ -41,7 +71,9 @@ define(["httpInterceptor", "angularMocks", "properties"], function(HttpIntercept
 
             var httpInterceptor = new HttpInterceptor(rootScope, q);
             var rejection = {
-                'key': 'value'
+                "config": {
+                    "url": "someurl"
+                }
             };
             rootScope.pendingRequests = 1;
             rootScope.loading = true;
@@ -50,6 +82,24 @@ define(["httpInterceptor", "angularMocks", "properties"], function(HttpIntercept
 
             expect(rootScope.loading).toEqual(false);
             expect(rootScope.pendingRequests).toEqual(0);
+            expect(q.reject).toHaveBeenCalledWith(rejection);
+        });
+
+        it("should not handle template request response errors", function() {
+            spyOn(q, "reject");
+            var httpInterceptor = new HttpInterceptor(rootScope, q);
+            var rejection = {
+                "config": {
+                    "url": "templates/blah"
+                }
+            };
+            rootScope.pendingRequests = 1;
+            rootScope.loading = true;
+
+            httpInterceptor.responseError(rejection);
+
+            expect(rootScope.loading).toEqual(true);
+            expect(rootScope.pendingRequests).toEqual(1);
             expect(q.reject).toHaveBeenCalledWith(rejection);
         });
 
