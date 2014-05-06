fa725394f1527e43aec392dfbf4b916beb95ffe9
sandeshds <sandeshdoolipeta@gmail.com>
2014-05-06 10:37:58 +0530

    Sandesh , Mahendra | #208 | Added tests for systemsettingtransformer

---
+++ src/test/unit/js/app/transformers/system.settings.transformer.spec.js
@@ -0,0 +1,39 @@
+define(["systemSettingsTransformer"], function(systemSettingsTransformer) {
+    describe("system settings", function() {
+
+        var modules, parent, expectedSystemSettings;
+
+
+        beforeEach(function() {
+
+            modules = [{
+                name: "test1",
+                id: 1,
+                selectedDataElements: {
+                    "123456": true,
+                    "123457": false,
+                    "123458": true,
+                    "123459": true,
+                    "123452": false,
+                    "123450": true,
+                    "123451": true,
+                }
+            }];
+
+            parent = {
+                name: "testParent",
+                id: 1
+            };
+
+            expectedSystemSettings = {
+                "excludedDataElements": {
+                    "1": ["123452", "123457"]
+                }
+            };
+        });
+
+        it("should construct system settings", function() {
+            expect(systemSettingsTransformer.constructSystemSettings(modules, parent)).toEqual(JSON.stringify(expectedSystemSettings));
+        });
+    });
+});
