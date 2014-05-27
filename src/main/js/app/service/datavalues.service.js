e94ac9e529fbf1726403ed105219498ef9bb004d
Aravind M <arav.psgtech@gmail.com>
2014-05-27 15:51:15 +0530

    aravind, mahendra | #102 | handling merge conflicts when uploading data values

--- src/main/js/app/service/datavalues.service.js
+++ src/main/js/app/service/datavalues.service.js
@@ -1,4 +1,4 @@
-define([], function() {
+define(["lodash", "moment"], function(_, moment) {
     return function(dataService, dataRepository, dataSetRepository, userPreferenceRepository, $q) {
         this.sync = function(dataToUpload) {
             var dataValues = [];
@@ -8,8 +8,51 @@ define([], function() {
                 return orgUnitIds.length > 0 && allDataSets.length > 0 ? dataService.downloadAllData(orgUnitIds, allDataSets) : [];
             };
 
+            var hasConflict = function(dataToUpload, downloadedData) {
+                var isBefore = function(date1, date2) {
+                    var d1 = moment(date1);
+                    var d2 = moment(date2);
+                    return d1.isBefore(d2);
+                };
+
+                return _.any(dataToUpload, function(udv) {
+                    return _.any(downloadedData, function(ddv) {
+                        return udv.dataElement === ddv.dataElement &&
+                            udv.period === ddv.period &&
+                            udv.orgUnit === ddv.orgUnit &&
+                            udv.categoryOptionCombo === ddv.categoryOptionCombo &&
+                            isBefore(udv.lastUpdated, ddv.lastUpdated);
+                    });
+                });
+            };
+
+            var merge = function(dataToUpload, downloadedData) {
+                var concatenatedDataValues = dataToUpload.dataValues.concat(downloadedData.dataValues);
+                var dataValuesUnion = _.transform(concatenatedDataValues, function(acc, dv) {
+                    var alreadyPresent = _.any(acc, {
+                        "dataElement": dv.dataElement,
+                        "period": dv.period,
+                        "orgUnit": dv.orgUnit,
+                        "categoryOptionCombo": dv.categoryOptionCombo
+                    });
+                    if (!alreadyPresent) {
+                        acc.push(dv);
+                    }
+                }, []);
+                return {
+                    "dataValues": dataValuesUnion
+                };
+            };
+
             var saveAllDataValues = function(data) {
                 console.debug("Storing data values : ", data);
+                if (dataToUpload && hasConflict(dataToUpload.dataValues, data.dataValues)) {
+                    console.debug("Conflicting data values : ", data, uploadData);
+                    dataRepository.save(data);
+                    return $q.reject("");
+                } else if (dataToUpload) {
+                    data = merge(dataToUpload, data);
+                }
                 return dataRepository.save(data);
             };
 
--- src/test/unit/js/app/service/datavalues.service.spec.js
+++ src/test/unit/js/app/service/datavalues.service.spec.js
@@ -26,7 +26,7 @@ define(["dataValuesService", "angularMocks", "properties", "utils", "dataService
                 }];
             }));
 
-            it("should sync data values if org units and datasets are present", function() {
+            it("should download data values if org units and datasets are present and there is no data to upload", function() {
                 spyOn(userPreferenceRepository, "getAll").and.returnValue(utils.getPromise(q, userPref));
                 spyOn(dataSetRepository, "getAll").and.returnValue(utils.getPromise(q, allDataSets));
                 spyOn(dataRepository, "save");
@@ -42,14 +42,55 @@ define(["dataValuesService", "angularMocks", "properties", "utils", "dataService
                 expect(dataService.save).not.toHaveBeenCalled();
             });
 
-            it("should download and upload datavalues", function() {
-                var dataValuesToUpload = [{
-                    "id": 1
-                }];
+            it("should upload datavalues if data to upload has no conflicts and save merged data values to idb", function() {
+                var dataValuesToUpload = {
+                    "dataValues": [{
+                        "id": 1,
+                        "dataElement": "DE1",
+                        "period": "2014W12",
+                        "orgUnit": "MSF_0",
+                        "categoryOptionCombo": "C1",
+                        "lastUpdated": "2014-05-27T09:25:37.120Z"
+                    }]
+                };
+                var downloadedDataValues = {
+                    "dataValues": [{
+                        "id": 1,
+                        "dataElement": "DE1",
+                        "period": "2014W12",
+                        "orgUnit": "MSF_0",
+                        "categoryOptionCombo": "C1",
+                        "lastUpdated": "2014-05-27T09:00:00.120Z"
+                    }, {
+                        "id": 2,
+                        "dataElement": "DE2",
+                        "period": "2014W12",
+                        "orgUnit": "MSF_0",
+                        "categoryOptionCombo": "C2",
+                        "lastUpdated": "2014-05-27T09:00:00.120Z"
+                    }]
+                };
+                var mergedDataValues = {
+                    "dataValues": [{
+                        "id": 1,
+                        "dataElement": "DE1",
+                        "period": "2014W12",
+                        "orgUnit": "MSF_0",
+                        "categoryOptionCombo": "C1",
+                        "lastUpdated": "2014-05-27T09:25:37.120Z"
+                    }, {
+                        "id": 2,
+                        "dataElement": "DE2",
+                        "period": "2014W12",
+                        "orgUnit": "MSF_0",
+                        "categoryOptionCombo": "C2",
+                        "lastUpdated": "2014-05-27T09:00:00.120Z"
+                    }]
+                };
                 spyOn(userPreferenceRepository, "getAll").and.returnValue(utils.getPromise(q, userPref));
                 spyOn(dataSetRepository, "getAll").and.returnValue(utils.getPromise(q, allDataSets));
                 spyOn(dataRepository, "save");
-                spyOn(dataService, "downloadAllData").and.returnValue(utils.getPromise(q, []));
+                spyOn(dataService, "downloadAllData").and.returnValue(utils.getPromise(q, downloadedDataValues));
                 spyOn(dataService, "save");
 
                 dataValuesService.sync(dataValuesToUpload);
@@ -58,7 +99,44 @@ define(["dataValuesService", "angularMocks", "properties", "utils", "dataService
                 expect(userPreferenceRepository.getAll).toHaveBeenCalled();
                 expect(dataSetRepository.getAll).toHaveBeenCalled();
                 expect(dataService.downloadAllData).toHaveBeenCalled();
+                expect(dataRepository.save).toHaveBeenCalledWith(mergedDataValues);
                 expect(dataService.save).toHaveBeenCalledWith(dataValuesToUpload);
+            });
+
+            it("should download but not upload datavalues if data to upload has conflicts", function() {
+                var dataValuesToUpload = {
+                    "dataValues": [{
+                        "id": 1,
+                        "dataElement": "DE1",
+                        "period": "2014W12",
+                        "orgUnit": "MSF_0",
+                        "categoryOptionCombo": "C1",
+                        "lastUpdated": "2014-05-27T08:25:37.120Z"
+                    }]
+                };
+                var downloadedDataValues = {
+                    "dataValues": [{
+                        "id": 1,
+                        "dataElement": "DE1",
+                        "period": "2014W12",
+                        "orgUnit": "MSF_0",
+                        "categoryOptionCombo": "C1",
+                        "lastUpdated": "2014-05-27T09:00:00.120Z"
+                    }]
+                };
+                spyOn(userPreferenceRepository, "getAll").and.returnValue(utils.getPromise(q, userPref));
+                spyOn(dataSetRepository, "getAll").and.returnValue(utils.getPromise(q, allDataSets));
+                spyOn(dataRepository, "save");
+                spyOn(dataService, "downloadAllData").and.returnValue(utils.getPromise(q, downloadedDataValues));
+                spyOn(dataService, "save");
+
+                dataValuesService.sync(dataValuesToUpload);
+                scope.$apply();
+
+                expect(userPreferenceRepository.getAll).toHaveBeenCalled();
+                expect(dataSetRepository.getAll).toHaveBeenCalled();
+                expect(dataService.downloadAllData).toHaveBeenCalled();
+                expect(dataService.save).not.toHaveBeenCalled();
             });
 
             it("should not sync data values if org units is not present", function() {
