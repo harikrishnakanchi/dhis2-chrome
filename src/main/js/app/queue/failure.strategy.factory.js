0e09c8c47f189517fd6f871e773f357d81ceec6c
Aravind M <arav.psgtech@gmail.com>
2014-05-28 17:13:15 +0530

    debug logs for retrying messages

--- src/main/js/app/queue/failure.strategy.factory.js
+++ src/main/js/app/queue/failure.strategy.factory.js
@@ -1,6 +1,7 @@
 define(["properties"], function(properties) {
     var create = function(hustle) {
         return function(message) {
+            console.debug("retrying message: id", message.id, "releases:", message.releases);
             if (message.releases < properties.queue.maxretries) {
                 hustle.Queue.release(message.id);
             } else {
