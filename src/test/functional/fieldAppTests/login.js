f04fccd055531b1af59111dca0616b39a9404e43
sandeshds <sandeshdoolipeta@gmail.com>
2014-06-05 19:44:13 +0530

    Sandesh | Refactored functional tests.

--- src/test/functional/fieldAppTests/login.js
+++ src/test/functional/fieldAppTests/login.js
@@ -1,12 +1,35 @@
-    login = function() {
-        var userName = element(by.id(username_textBox));
-        var password = element(by.id(password_textBox));
-        var logoutLink = element(by.id(logout_link));
-        var loginButton = element(by.id(login_button));
-        var projectLink = element(by.id(dashboard_project_link));
+setUpLoginData = function() {
+    userName = element(by.id(username_textBox));
+    password = element(by.id(password_textBox));
+    logoutLink = element(by.id(logout_link));
+    loginButton = element(by.id(login_button));
+    projectLink = element(by.id(dashboard_project_link));
+    invalidLoginMsg = element(by.id(invalid_login_msg_id));
+};
 
-        userName.sendKeys(admin_username);
-        password.sendKeys(admin_password);
-        loginButton.click();
-        return;
-    };
+loginAsAdmin = function() {
+    userName.sendKeys(admin_username);
+    password.sendKeys(admin_password);
+
+    loginButton.click();
+    ptor.waitForAngular();
+
+    expect(projectLink.getText()).toEqual('Projects');
+
+    projectLink.click();
+};
+
+logout = function() {
+    logoutLink.click();
+
+    expect(loginButton.isPresent()).toBe(true);
+};
+
+loginAsAdminWithInvalidCredentials = function() {
+    userName.sendKeys(admin_username);
+    password.sendKeys(incorrect_password);
+    loginButton.click();
+
+    expect(invalidLoginMsg.getText()).toEqual(invalid_login_error_msg);
+    expect(loginButton.isPresent()).toBe(true);
+};
--- src/test/functional/fieldAppTests/loginPageTest.spec.js
+++ src/test/functional/fieldAppTests/loginPageTest.spec.js
@@ -5,6 +5,7 @@ describe('The admin ', function() {
     beforeEach(function() {
         ptor = protractor.getInstance();
         browser.get('http://localhost:8081/index.html#/dashboard');
+        setUpLoginData();
     });
 
     function writeScreenShot(data, filename) {
@@ -15,36 +16,12 @@ describe('The admin ', function() {
     }
 
     it('should be able to login with correct password', function() {
-        var userName = element(by.id(username_textBox));
-        var password = element(by.id(password_textBox));
-        var logoutLink = element(by.id(logout_link));
-        var loginButton = element(by.id(login_button));
-        var projectLink = element(by.id(dashboard_project_link));
-
-        // userName.sendKeys(admin_username);
-        // password.sendKeys(admin_password);
-        // loginButton.click();
-        login();
-        ptor.waitForAngular();
-        expect(projectLink.getText()).toEqual('Projects');
-
-        projectLink.click();
-        logoutLink.click();
-        expect(loginButton.isPresent()).toBe(true);
+        loginAsAdmin();
+        logout();
     });
 
     it('should not be able to login with incorrect password', function() {
-        var userName = element(by.id(username_textBox));
-        var password = element(by.id(password_textBox));
-        var loginButton = element(by.id(login_button));
-        var invalidLoginMsg = element(by.id(invalid_login_msg_id));
-
-        userName.sendKeys(admin_username);
-        password.sendKeys(incorrect_password);
-
-        loginButton.click();
-        expect(invalidLoginMsg.getText()).toEqual(invalid_login_error_msg);
-        expect(loginButton.isPresent()).toBe(true);
+        loginAsAdminWithInvalidCredentials();
     });
 
     afterEach(function() {
