a3a6558add2b0f160440c73b9b46ef74d6b270bd
Shashank Raghavendra <shashanr@thoughtworks.com>
2014-06-19 12:08:25 +0530

    shashank | adding dataEntryPage tests

---
+++ src/test/functional/fieldAppTests/pages/genericMethods.js
@@ -0,0 +1,9 @@
+       // selectDropdownbyNum = function(element, optionNum) {
+       //      if (optionNum) {
+       //          var options = element.findElements(by.tagName('option'))
+       //              .then(function(options) {
+       //                  ptor.waitForAngular();
+       //                  options[optionNum].click();
+       //              });
+       //      }
+       //  };
