1909a7edb5e5c0b313e96fec4670ce744853dbe4
mahendrakariya <mahendrakariya@gmail.com>
2014-07-07 10:58:32 +0530

    Simanta,Mahendra | #383  | adding approvalDataRepo to bgRepos

--- src/main/js/app/repository/bg.repositories.js
+++ src/main/js/app/repository/bg.repositories.js
@@ -1,8 +1,9 @@
-define(["dataRepository", "dataSetRepository", "userPreferenceRepository"], function(dataRepository, dataSetRepository, userPreferenceRepository) {
+define(["dataRepository", "dataSetRepository", "userPreferenceRepository", "approvalDataRepository"], function(dataRepository, dataSetRepository, userPreferenceRepository, approvalDataRepository) {
     var init = function(app) {
         app.service('dataRepository', ['$indexedDB', dataRepository]);
         app.service('dataSetRepository', ['$indexedDB', dataSetRepository]);
         app.service('userPreferenceRepository', ['$indexedDB', userPreferenceRepository]);
+        app.service('approvalDataRepository', ['$indexedDB', approvalDataRepository]);
     };
     return {
         init: init
