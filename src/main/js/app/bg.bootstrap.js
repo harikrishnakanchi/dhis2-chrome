8ac128c8358ae4a1f662eb4f176dbd02db04a6cc
Aravind M <arav.psgtech@gmail.com>
2014-07-15 11:24:46 +0530

    Simij,Aravind | #00 | not bootstrapping the app again if it is already bootstrapped

--- src/main/js/app/bg.bootstrap.js
+++ src/main/js/app/bg.bootstrap.js
@@ -6,12 +6,16 @@ require(["app/bg.config"], function(config) {
     require(["indexedDBLogger", "app"], function(indexedDBLogger, app) {
         indexedDBLogger.configure("msfLogs");
         require(["properties"], function(properties) {
+            var bootstrapData;
             var onMigrationComplete = function(request, sender, sendResponse) {
-                if (request === "migrationComplete") {
+                if (!bootstrapData && request === "migrationComplete") {
                     console.log("dB migration complete");
-                    app.bootstrap(app.init());
+                    app.bootstrap(app.init()).then(function(data) {
+                        bootstrapData = data;
+                    });
                 }
             };
+
             chrome.runtime.onMessage.addListener(onMigrationComplete);
             chrome.app.runtime.onLaunched.addListener(function(launchData) {
                 chrome.app.window.create('../../index.html', {
