051a8aecc77b8060110349d919f5285dd66f1d7c
mahendrakariya <mahendrakariya@gmail.com>
2014-10-10 16:21:46 +0530

    Mahendra | #00 | Fixing indentation

--- src/main/js/app/controller/dashboard.controller.js
+++ src/main/js/app/controller/dashboard.controller.js
@@ -1,20 +1,20 @@
-    define([], function() {
-        return function($scope, $hustle, $q) {
-            var dataValues = [];
+define([], function() {
+    return function($scope, $hustle, $q) {
+        var dataValues = [];
 
-            $scope.syncNow = function() {
-                $scope.isSyncRunning = true;
+        $scope.syncNow = function() {
+            $scope.isSyncRunning = true;
 
-                var onSuccess = function(response) {
-                    $scope.isSyncRunning = false;
-                    $scope.isSyncDone = true;
-                };
-
-                var downloadData = $hustle.publish({
-                    "type": "downloadData"
-                }, "dataValues");
-
-                return downloadData.then(onSuccess);
+            var onSuccess = function(response) {
+                $scope.isSyncRunning = false;
+                $scope.isSyncDone = true;
             };
+
+            var downloadData = $hustle.publish({
+                "type": "downloadData"
+            }, "dataValues");
+
+            return downloadData.then(onSuccess);
         };
-    });
+    };
+});
