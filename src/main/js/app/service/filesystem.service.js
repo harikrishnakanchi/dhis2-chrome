7ff24977415089c0c6afd0d70c47560377934a00
mahendrakariya <mahendrakariya@gmail.com>
2014-12-04 10:39:20 +0530

    Mahendra | #66 | filesystem util to read files

--- src/main/js/app/service/filesystem.service.js
+++ src/main/js/app/service/filesystem.service.js
@@ -1,23 +1,21 @@
 define([], function() {
     return function() {
         var writeFile = function(fileName, contents, blobType, successHandler, errorHandler) {
-            console.log(fileName);
             var entryType = {
-                type: 'openDirectory'
+                "type": 'openDirectory'
             };
 
             var createStrategy = {
-                create: true,
-                exclusive: true
+                "create": true,
+                "exclusive": true
             };
 
             chrome.fileSystem.chooseEntry(entryType, function(entry) {
                 chrome.fileSystem.getWritableEntry(entry, function(writableEntry) {
                     writableEntry.getFile(fileName, createStrategy, function(fileEntry) {
                         fileEntry.createWriter(function(writer) {
-                            writer.onerror = function(e) {
-                                errorHandler(e);
-                            };
+
+                            writer.onerror = errorHandler;
                             writer.onwriteend = function() {
                                 successHandler(entry);
                             };
@@ -30,8 +28,24 @@ define([], function() {
             });
         };
 
+        var readFile = function(successHandler, errorHandler) {
+            var entryType = {
+                "type": 'openFile'
+            };
+
+            chrome.fileSystem.chooseEntry(entryType, function(readOnlyEntry) {
+                readOnlyEntry.file(function(file) {
+                    var reader = new FileReader();
+                    reader.onerror = errorHandler;
+                    reader.onloadend = successHandler;
+                    reader.readAsText(file);
+                });
+            });
+        };
+
         return {
-            "writeFile": writeFile
+            "writeFile": writeFile,
+            "readFile": readFile
         };
     };
 });
