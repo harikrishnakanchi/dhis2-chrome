980e4f5653ee7e0d4fd3dd2f7223c57f1247bbf3
Mansi Singhal <singhalmansi7@gmail.com>
2014-12-29 11:34:56 +0530

    Sandesh,MAnsi | Refactoring date utils to use isoWeekYear instead of year method

--- src/main/js/app/utils/date.utils.js
+++ src/main/js/app/utils/date.utils.js
@@ -1,6 +1,6 @@
 define(["moment"], function(moment) {
     var toDhisFormat = function(m) {
-        return m.year() + "W" + m.isoWeek();
+        return m.isoWeekYear() + "W" + m.isoWeek();
     };
 
     return {
