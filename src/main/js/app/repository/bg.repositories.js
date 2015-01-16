38887112c9b9dd2e92d64ef7509404ab5e0d0f15
mahendrakariya <mahendrakariya@gmail.com>
2015-01-16 21:50:44 +0530

    Mahendra | #00 | fixing org unit sync; added missing dependencies in bg.repositories

--- src/main/js/app/repository/bg.repositories.js
+++ src/main/js/app/repository/bg.repositories.js
@@ -2,7 +2,7 @@ define(["dataRepository", "dataSetRepository", "userPreferenceRepository", "appr
     var init = function(app) {
         app.service('dataRepository', ['$indexedDB', dataRepository]);
         app.service('dataSetRepository', ['$indexedDB', dataSetRepository]);
-        app.service('orgUnitRepository', ['$indexedDB', orgUnitRepository]);
+        app.service('orgUnitRepository', ['$indexedDB', 'dataSetRepository', '$q', orgUnitRepository]);
         app.service('userPreferenceRepository', ['$indexedDB', 'orgUnitRepository', userPreferenceRepository]);
         app.service('approvalDataRepository', ['$indexedDB', approvalDataRepository]);
         app.service('programEventRepository', ['$indexedDB', '$q', programEventRepository]);
