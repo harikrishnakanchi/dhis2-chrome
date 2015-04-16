c8b1772e0414b4bd1ebb996b8e2cdd3a59fc8e9d
Simanta J Handique <shandique@thoughtworks.com>
2015-04-16 18:33:32 +0530

    Simanta | #000 | Tests for new dataset transformer

--- src/test/unit/js/app/transformers/dataset.transformer.spec.js
+++ src/test/unit/js/app/transformers/dataset.transformer.spec.js
@@ -1,6 +1,180 @@
 define(["datasetTransformer", "testData", "lodash"], function(datasetTransformer, testData, _) {
-    xdescribe("datasetTransformer", function() {
-        it("should enrich datasets", function() {
+    describe("datasetTransformer", function() {
+
+        it("should map aggregate dataset for view", function() {
+            var aggregateDataset = {
+                "id": "ds1",
+                "name": "Aggregate Dataset",
+                "organisationUnits": [{
+                    "id": "ou1"
+                }],
+                "attributeValues": [{
+                    "attribute": {
+                        "code": "isNewDataModel"
+                    },
+                    "value": "true"
+                }, {
+                    "attribute": {
+                        "code": "isOriginDataset"
+                    },
+                    "value": "false"
+                }, {
+                    "attribute": {
+                        "code": "isLineListService"
+                    },
+                    "value": "false"
+                }]
+            };
+
+            var aggregateDatasetForView = datasetTransformer.mapDatasetForView(aggregateDataset);
+
+            var expectedDataset = {
+                "id": "ds1",
+                "name": "Aggregate Dataset",
+                "organisationUnits": [{
+                    "id": "ou1"
+                }],
+                "isAggregateService": true,
+                "isLineListService": false,
+                "isOriginDataset": false,
+                "isNewDataModel": true
+            };
+
+            expect(aggregateDatasetForView).toEqual(expectedDataset);
+        });
+
+        it("should map linelist dataset for view", function() {
+            var linelistDataset = {
+                "id": "ds1",
+                "name": "Line List Dataset",
+                "organisationUnits": [{
+                    "id": "ou1"
+                }],
+                "attributeValues": [{
+                    "attribute": {
+                        "code": "isNewDataModel"
+                    },
+                    "value": "true"
+                }, {
+                    "attribute": {
+                        "code": "isOriginDataset"
+                    },
+                    "value": "false"
+                }, {
+                    "attribute": {
+                        "code": "isLineListService"
+                    },
+                    "value": "true"
+                }]
+            };
+
+            var linelistDatasetForView = datasetTransformer.mapDatasetForView(linelistDataset);
+
+            var expectedDataset = {
+                "id": "ds1",
+                "name": "Line List Dataset",
+                "organisationUnits": [{
+                    "id": "ou1"
+                }],
+                "isAggregateService": false,
+                "isLineListService": true,
+                "isOriginDataset": false,
+                "isNewDataModel": true
+            };
+
+            expect(linelistDatasetForView).toEqual(expectedDataset);
+        });
+
+        it("should map origin dataset for view", function() {
+            var originDataset = {
+                "id": "ds1",
+                "name": "Origin Dataset",
+                "organisationUnits": [{
+                    "id": "ou1"
+                }, {
+                    "id": "ou2"
+                }],
+                "attributeValues": [{
+                    "attribute": {
+                        "code": "isNewDataModel"
+                    },
+                    "value": "true"
+                }, {
+                    "attribute": {
+                        "code": "isOriginDataset"
+                    },
+                    "value": "true"
+                }, {
+                    "attribute": {
+                        "code": "isLineListService"
+                    },
+                    "value": "false"
+                }]
+            };
+
+            var originDatasetForView = datasetTransformer.mapDatasetForView(originDataset);
+
+            var expectedDataset = {
+                "id": "ds1",
+                "name": "Origin Dataset",
+                "organisationUnits": [{
+                    "id": "ou1"
+                }, {
+                    "id": "ou2"
+                }],
+                "isAggregateService": false,
+                "isLineListService": false,
+                "isOriginDataset": true,
+                "isNewDataModel": true
+            };
+
+            expect(originDatasetForView).toEqual(expectedDataset);
+        });
+
+        it("should map current dataset for view", function() {
+
+            var currentDataset = {
+                "id": "ds1",
+                "name": "V1 Dataset",
+                "organisationUnits": [{
+                    "id": "ou1"
+                }],
+                "attributeValues": [{
+                    "attribute": {
+                        "code": "isNewDataModel"
+                    },
+                    "value": "false"
+                }, {
+                    "attribute": {
+                        "code": "isOriginDataset"
+                    },
+                    "value": "false"
+                }, {
+                    "attribute": {
+                        "code": "isLineListService"
+                    },
+                    "value": "false"
+                }]
+            };
+
+            var currentDatasetForView = datasetTransformer.mapDatasetForView(currentDataset);
+
+            var expectedDataset = {
+                "id": "ds1",
+                "name": "V1 Dataset",
+                "organisationUnits": [{
+                    "id": "ou1"
+                }],
+                "isAggregateService": true,
+                "isLineListService": false,
+                "isOriginDataset": false,
+                "isNewDataModel": false
+            };
+
+            expect(currentDatasetForView).toEqual(expectedDataset);
+        });
+
+        it("should enrich datasets with sections and dataelements", function() {
 
             var datasets, sections, dataelements;
             var allDataSetsFromTestData = testData.get("dataSets");
@@ -75,7 +249,7 @@ define(["datasetTransformer", "testData", "lodash"], function(datasetTransformer
                 }]
             }];
 
-            var actualEnrichedDatasets = datasetTransformer.enrichDatasets(datasets, sections, dataelements, ['DE3']);
+            var actualEnrichedDatasets = datasetTransformer.enrichWithSectionsAndDataElements(datasets, sections, dataelements, ['DE3']);
             expect(actualEnrichedDatasets.length).toBe(2);
             expect(actualEnrichedDatasets[0].sections.length).toBe(2);
             expect(actualEnrichedDatasets[0].sections).toContain(expectedSectionsForOpd[0]);
@@ -84,48 +258,50 @@ define(["datasetTransformer", "testData", "lodash"], function(datasetTransformer
             expect(actualEnrichedDatasets[1].sections).toContain(expectedSectionsForVacc[0]);
         });
 
-        it("should get datasets associated with org units", function() {
-
-            var dataset1 = {
-                "id": "DS1",
-                "organisationUnits": [{
-                    "name": "Mod1",
-                    "id": "Mod1Id"
-                }, {
-                    "name": "Mod2",
-                    "id": "Mod2Id"
+        it("should enrich datasets with category option combinations", function() {
+            var dataSets = [{
+                "id": "DS_OPD",
+                "sections": [{
+                    "id": "Sec1",
+                    "dataElements": [{
+                        "id": "DE1",
+                        "categoryCombo": {
+                            "id": "CC1"
+                        }
+                    }]
                 }]
-            };
-            var dataset2 = {
-                "id": "DS2",
-                "organisationUnits": [{
-                    "name": "Mod3",
-                    "id": "Mod3Id"
+            }];
+
+            var allCategoryCombos = testData.get("categoryCombos");
+            var allCategories = testData.get("categories");
+            var allCategoryOptionCombos = testData.get("categoryOptionCombos");
+            var enrichedDatasets = datasetTransformer.enrichWithCategoryOptionCombinations(dataSets, allCategoryCombos, allCategories, allCategoryOptionCombos)
+
+            var expectedSectionHeaders = [
+                [{
+                    "id": "CO1",
+                    "name": "Resident"
                 }, {
-                    "name": "Mod2",
-                    "id": "Mod2Id"
-                }]
-            };
-            var dataset3 = {
-                "id": "DS3",
-                "organisationUnits": [{
-                    "name": "Mod3",
-                    "id": "Mod3Id"
+                    "id": "CO2",
+                    "name": "Migrant"
+                }],
+                [{
+                    "id": "CO3",
+                    "name": "LessThan5"
                 }, {
-                    "name": "Mod1",
-                    "id": "Mod1Id"
+                    "id": "CO4",
+                    "name": "GreaterThan5"
+                }, {
+                    "id": "CO3",
+                    "name": "LessThan5"
+                }, {
+                    "id": "CO4",
+                    "name": "GreaterThan5"
                 }]
-            };
-            var orgUnit = {
-                "name": "Mod2",
-                "id": "Mod2Id"
-            };
-
-            var datasets = [dataset1, dataset2];
-
-            var actualDatasets = datasetTransformer.getAssociatedDatasets(orgUnit.id, datasets);
-
-            expect(actualDatasets).toEqual([dataset1, dataset2]);
+            ];
+            expect(enrichedDatasets[0].sections[0].headers).toEqual(expectedSectionHeaders);
+            expect(enrichedDatasets[0].sections[0].categoryOptionComboIds).toEqual(['1', '2', '3', '4']);
         });
+
     });
 });
