da228fbad13f65f8c2d8125cc39e067411030b1e
Simanta J Handique <shandique@thoughtworks.com>
2015-07-23 16:32:43 +0530

    Simanta | #000 | Splitting up getApprovalData calls to avoid timeouts

--- src/main/js/app/consumer/download.approval.consumer.js
+++ src/main/js/app/consumer/download.approval.consumer.js
@@ -1,92 +1,103 @@
-define(["moment", "properties", "lodash", "dateUtils"], function(moment, properties, _, dateUtils) {
-    return function(datasetRepository, userPreferenceRepository, orgUnitRepository, $q, approvalService, approvalDataRepository) {
+    define(["moment", "properties", "lodash", "dateUtils"], function(moment, properties, _, dateUtils) {
+        return function(datasetRepository, userPreferenceRepository, orgUnitRepository, $q, approvalService, approvalDataRepository) {
 
-        var getMetadata = function() {
-            return $q.all([userPreferenceRepository.getUserModuleIds(), datasetRepository.getAll()]).then(function(data) {
-                var userModuleIds = data[0];
-                var allDataSetIds = _.pluck(data[1], "id");
-                var originOrgUnits = [];
-
-                if (_.isEmpty(userModuleIds))
-                    return [userModuleIds, originOrgUnits, allDataSetIds];
-
-                return orgUnitRepository.findAllByParent(userModuleIds).then(function(originOrgUnits) {
-                    return [userModuleIds, originOrgUnits, allDataSetIds];
+            var getDatasetsForModule = function(moduleId) {
+                return orgUnitRepository.findAllByParent([moduleId]).then(function(originOrgUnits) {
+                    var moduleAndOriginOrgUnitIds = _.pluck(originOrgUnits, "id").concat(moduleId);
+                    return datasetRepository.findAllForOrgUnits(moduleAndOriginOrgUnitIds);
                 });
-            });
-        };
+            };
 
-        var getCompletionAndApprovalData = function(metadata) {
-            var userModuleIds = metadata[0];
-            var originOrgUnits = metadata[1];
-            var allDataSetIds = metadata[2];
+            var getMetadata = function() {
+                return $q.all([userPreferenceRepository.getUserModuleIds(), datasetRepository.getAll()]).then(function(data) {
+                    var userModuleIds = data[0];
+                    var allDataSetIds = _.pluck(data[1], "id");
+                    var originOrgUnits = [];
 
-            if (userModuleIds.length === 0 || allDataSetIds.length === 0)
-                return;
+                    if (_.isEmpty(userModuleIds))
+                        return [userModuleIds, originOrgUnits, allDataSetIds];
 
-            var m = moment();
-            var startPeriod = dateUtils.toDhisFormat(m.isoWeek(m.isoWeek() - properties.projectDataSync.numWeeksToSync + 1));
-            var endPeriod = dateUtils.toDhisFormat(moment());
-
-            var getDhisCompletionDataPromise = approvalService.getCompletionData(userModuleIds, originOrgUnits, allDataSetIds);
-
-            var getDhisApprovalDataPromise = approvalService.getApprovalData(userModuleIds, allDataSetIds);
-
-            var getApprovalDataPromise = approvalDataRepository.getApprovalDataForPeriodsOrgUnits(startPeriod, endPeriod, userModuleIds);
-
-            return $q.all([getDhisCompletionDataPromise, getDhisApprovalDataPromise, getApprovalDataPromise]);
-        };
-
-        var saveCompletionAndApprovalData = function(data) {
-            if (!data)
-                return;
-
-            var dhisCompletionList = data[0];
-            var dhisApprovalList = data[1];
-            var dbApprovalList = data[2];
-
-            var approvalUpsertPromises = [];
-
-            var mergedDhisCompletionAndApprovalList = _.map(dhisCompletionList, function(dhisCompletionData) {
-                var dhisApprovalData = _.find(dhisApprovalList, _.matches({
-                    "period": dhisCompletionData.period,
-                    "orgUnit": dhisCompletionData.orgUnit
-                }));
-                if (dhisApprovalData)
-                    return _.merge(dhisCompletionData, dhisApprovalData);
-                return dhisCompletionData;
-            });
-
-            var newApprovals = _.reject(mergedDhisCompletionAndApprovalList, function(dhisApprovalData) {
-                return _.any(dbApprovalList, {
-                    "orgUnit": dhisApprovalData.orgUnit,
-                    "period": dhisApprovalData.period
+                    return orgUnitRepository.findAllByParent(userModuleIds).then(function(originOrgUnits) {
+                        return [userModuleIds, originOrgUnits, allDataSetIds];
+                    });
                 });
-            });
+            };
 
-            if (!_.isEmpty(newApprovals))
-                approvalUpsertPromises.push(approvalDataRepository.saveApprovalsFromDhis(newApprovals));
+            var getCompletionAndApprovalData = function(metadata) {
+                var userModuleIds = metadata[0];
+                var originOrgUnits = metadata[1];
+                var allDataSetIds = metadata[2];
 
-            _.each(dbApprovalList, function(dbApproval) {
-                if (dbApproval.status === "NEW" || dbApproval.status === "DELETED")
+                if (userModuleIds.length === 0 || allDataSetIds.length === 0)
                     return;
 
-                var dhisApprovalData = _.find(mergedDhisCompletionAndApprovalList, {
-                    "orgUnit": dbApproval.orgUnit,
-                    "period": dbApproval.period
+                var m = moment();
+                var startPeriod = dateUtils.toDhisFormat(m.isoWeek(m.isoWeek() - properties.projectDataSync.numWeeksToSync + 1));
+                var endPeriod = dateUtils.toDhisFormat(moment());
+
+                var getDhisCompletionDataPromise = approvalService.getCompletionData(userModuleIds, originOrgUnits, allDataSetIds);
+
+                var getDhisApprovalDataPromises = _.map(userModuleIds, function(moduleId) {
+                    return getDatasetsForModule(moduleId).then(function(datasets) {
+                        return approvalService.getApprovalData(moduleId, _.pluck(datasets, "id"));
+                    });
                 });
 
-                var l1UpdatePromise = dhisApprovalData ? approvalDataRepository.saveApprovalsFromDhis(dhisApprovalData) : approvalDataRepository.invalidateApproval(dbApproval.period, dbApproval.orgUnit);
-                approvalUpsertPromises.push(l1UpdatePromise);
-            });
+                var getApprovalDataPromise = approvalDataRepository.getApprovalDataForPeriodsOrgUnits(startPeriod, endPeriod, userModuleIds);
 
-            return $q.all(approvalUpsertPromises);
-        };
+                return $q.all([getDhisCompletionDataPromise, $q.all(getDhisApprovalDataPromises), getApprovalDataPromise]);
+            };
 
-        this.run = function() {
-            return getMetadata()
-                .then(getCompletionAndApprovalData)
-                .then(saveCompletionAndApprovalData);
+            var saveCompletionAndApprovalData = function(data) {
+                if (!data)
+                    return;
+
+                var dhisCompletionList = data[0];
+                var dhisApprovalList = _.flatten(data[1]);
+                var dbApprovalList = data[2];
+
+                var approvalUpsertPromises = [];
+
+                var mergedDhisCompletionAndApprovalList = _.map(dhisCompletionList, function(dhisCompletionData) {
+                    var dhisApprovalData = _.find(dhisApprovalList, _.matches({
+                        "period": dhisCompletionData.period,
+                        "orgUnit": dhisCompletionData.orgUnit
+                    }));
+                    if (dhisApprovalData)
+                        return _.merge(dhisCompletionData, dhisApprovalData);
+                    return dhisCompletionData;
+                });
+
+                var newApprovals = _.reject(mergedDhisCompletionAndApprovalList, function(dhisApprovalData) {
+                    return _.any(dbApprovalList, {
+                        "orgUnit": dhisApprovalData.orgUnit,
+                        "period": dhisApprovalData.period
+                    });
+                });
+
+                if (!_.isEmpty(newApprovals))
+                    approvalUpsertPromises.push(approvalDataRepository.saveApprovalsFromDhis(newApprovals));
+
+                _.each(dbApprovalList, function(dbApproval) {
+                    if (dbApproval.status === "NEW" || dbApproval.status === "DELETED")
+                        return;
+
+                    var dhisApprovalData = _.find(mergedDhisCompletionAndApprovalList, {
+                        "orgUnit": dbApproval.orgUnit,
+                        "period": dbApproval.period
+                    });
+
+                    var l1UpdatePromise = dhisApprovalData ? approvalDataRepository.saveApprovalsFromDhis(dhisApprovalData) : approvalDataRepository.invalidateApproval(dbApproval.period, dbApproval.orgUnit);
+                    approvalUpsertPromises.push(l1UpdatePromise);
+                });
+
+                return $q.all(approvalUpsertPromises);
+            };
+
+            this.run = function() {
+                return getMetadata()
+                    .then(getCompletionAndApprovalData)
+                    .then(saveCompletionAndApprovalData);
+            };
         };
-    };
-});
+    });
