62ea68c9b7f83187d84ec6a91d742020de64a08c
raghavcreddy <raghavr@thoughtworks.com>
2015-11-30 16:08:23 +0530

    Raghavendra, ShilpaG | #1489 | Recursively downloaded events for origin ids for linelist data

--- src/test/unit/js/app/consumers/download.event.data.consumer.spec.js
+++ src/test/unit/js/app/consumers/download.event.data.consumer.spec.js
@@ -7,25 +7,39 @@ define(["downloadEventDataConsumer", "angularMocks", "properties", "utils", "eve
                 scope = $rootScope.$new();
 
                 userPreferenceRepository = new UserPreferenceRepository();
-                spyOn(userPreferenceRepository, "getOriginOrgUnitIds").and.returnValue(utils.getPromise(q, ["mod1"]));
+                spyOn(userPreferenceRepository, "getOriginOrgUnitIds").and.returnValue(utils.getPromise(q, ["origin1", "origin2", "origin3", "origin4", "origin5"]));
                 eventService = new EventService();
                 programEventRepository = new ProgramEventRepository();
                 downloadEventDataConsumer = new DownloadEventDataConsumer(eventService, programEventRepository, userPreferenceRepository, q);
+
             }));
 
             it("should download events from dhis and save them to indexeddb", function() {
-                var dhisEventList = {
-                    'events': [{
-                        'event': 'e1',
-                        'eventDate': '2015-06-28T18:30:00.000Z'
-                    }]
+                var dhisEventList = function(index) {
+                    return {
+                        'events': [{
+                            'event': 'e' + index,
+                            'eventDate': '2015-06-28T18:30:00.000Z'
+                        }]
+                    };
                 };
 
                 spyOn(programEventRepository, "isDataPresent").and.returnValue(utils.getPromise(q, true));
-                spyOn(eventService, "getRecentEvents").and.returnValue(utils.getPromise(q, dhisEventList));
                 spyOn(programEventRepository, "getEventsFromPeriod").and.returnValue(utils.getPromise(q, []));
                 spyOn(programEventRepository, "delete").and.returnValue(utils.getPromise(q, []));
                 spyOn(programEventRepository, "upsert");
+                spyOn(eventService, "getRecentEvents").and.callFake(function(startDate, orgUnitId) {
+                    if (orgUnitId == "origin1")
+                        return utils.getPromise(q, dhisEventList(1));
+                    if (orgUnitId == "origin2")
+                        return utils.getPromise(q, dhisEventList(2));
+                    if (orgUnitId == "origin3")
+                        return utils.getPromise(q, dhisEventList(3));
+                    if (orgUnitId == "origin4")
+                        return utils.getPromise(q, dhisEventList(4));
+                    if (orgUnitId == "origin5")
+                        return utils.getPromise(q, dhisEventList(5));
+                });
 
                 var message = {
                     "data": {
@@ -37,11 +51,77 @@ define(["downloadEventDataConsumer", "angularMocks", "properties", "utils", "eve
                 scope.$apply();
 
                 var expectedEventPayload = [{
+                    'event': 'e5',
+                    'eventDate': '2015-06-28T18:30:00.000Z'
+                }, {
+                    'event': 'e4',
+                    'eventDate': '2015-06-28T18:30:00.000Z'
+                }, {
+                    'event': 'e3',
+                    'eventDate': '2015-06-28T18:30:00.000Z'
+                }, {
+                    'event': 'e2',
+                    'eventDate': '2015-06-28T18:30:00.000Z'
+                }, {
                     'event': 'e1',
                     'eventDate': '2015-06-28T18:30:00.000Z'
                 }];
 
-                expect(eventService.getRecentEvents).toHaveBeenCalled();
+                expect(eventService.getRecentEvents.calls.count()).toEqual(5);
+                expect(programEventRepository.upsert).toHaveBeenCalledWith(expectedEventPayload);
+            });
+
+            it("should continue to download events from dhis even if one call fails", function() {
+                var dhisEventList = function(index) {
+                    return {
+                        'events': [{
+                            'event': 'e' + index,
+                            'eventDate': '2015-06-28T18:30:00.000Z'
+                        }]
+                    };
+                };
+
+                spyOn(programEventRepository, "isDataPresent").and.returnValue(utils.getPromise(q, true));
+                spyOn(programEventRepository, "getEventsFromPeriod").and.returnValue(utils.getPromise(q, []));
+                spyOn(programEventRepository, "delete").and.returnValue(utils.getPromise(q, []));
+                spyOn(programEventRepository, "upsert");
+                spyOn(eventService, "getRecentEvents").and.callFake(function(startDate, orgUnitId) {
+                    if (orgUnitId == "origin1")
+                        return utils.getPromise(q, dhisEventList(1));
+                    if (orgUnitId == "origin2")
+                        return utils.getPromise(q, dhisEventList(2));
+                    if (orgUnitId == "origin3")
+                        return utils.getRejectedPromise(q, dhisEventList(3));
+                    if (orgUnitId == "origin4")
+                        return utils.getPromise(q, dhisEventList(4));
+                    if (orgUnitId == "origin5")
+                        return utils.getPromise(q, dhisEventList(5));
+                });
+
+                var message = {
+                    "data": {
+                        "type": "downloadEventData"
+                    }
+                };
+
+                downloadEventDataConsumer.run(message);
+                scope.$apply();
+
+                var expectedEventPayload = [{
+                    'event': 'e5',
+                    'eventDate': '2015-06-28T18:30:00.000Z'
+                }, {
+                    'event': 'e4',
+                    'eventDate': '2015-06-28T18:30:00.000Z'
+                }, {
+                    'event': 'e2',
+                    'eventDate': '2015-06-28T18:30:00.000Z'
+                }, {
+                    'event': 'e1',
+                    'eventDate': '2015-06-28T18:30:00.000Z'
+                }];
+
+                expect(eventService.getRecentEvents.calls.count()).toEqual(5);
                 expect(programEventRepository.upsert).toHaveBeenCalledWith(expectedEventPayload);
             });
 
