ab64c1d1ba3537387c59cc25e7bc8e7e6a05bb89
David Oh <doh@thoughtworks.com>
2016-02-19 14:26:04 +0400

    David | #1663 | Refactored spec for getAll method in chartRepository

--- src/test/unit/js/app/repository/chart.repository.spec.js
+++ src/test/unit/js/app/repository/chart.repository.spec.js
@@ -1,4 +1,4 @@
-define(["chartRepository", "angularMocks", "utils"], function(ChartRepository, mocks, utils) {
+define(["chartRepository", "angularMocks", "utils", "lodash"], function(ChartRepository, mocks, utils, _) {
     describe('Chart Repository', function() {
         var mockStore, chartRepository, q, mockDB;
 
@@ -10,6 +10,26 @@ define(["chartRepository", "angularMocks", "utils"], function(ChartRepository, m
 
             chartRepository = new ChartRepository(mockDB.db, q);
         }));
+
+        describe('getAll', function() {
+            it('should get all the charts', function() {
+                var allCharts = [{
+                    'id': 'chart1',
+                    'name': '[FieldApp - dataSetCode] # Name'
+                }, {
+                    'id': 'chart2',
+                    'name': '[FieldApp - dataSetCode] # Name'
+                }];
+                mockStore.getAll.and.returnValue(utils.getPromise(q, allCharts));
+
+                chartRepository.getAll().then(function(chartsFromRepository) {
+                    expect(_.pluck(chartsFromRepository, 'id')).toEqual(['chart1', 'chart2']);
+                });
+                scope.$apply();
+
+                expect(mockStore.getAll).toHaveBeenCalled();
+            });
+        });
 
         it('should upsert the charts', function() {
             var chartsToUpsert = [{
@@ -64,11 +84,6 @@ define(["chartRepository", "angularMocks", "utils"], function(ChartRepository, m
             }, 'orgUnitId').then(function(result) {
                 expect(result).toEqual(data);
             });
-        });
-
-        it('should get All the charts', function() {
-            chartRepository.getAll();
-            expect(mockStore.getAll).toHaveBeenCalled();
         });
 
         it('should get All notification charts', function() {
