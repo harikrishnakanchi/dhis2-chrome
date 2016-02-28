c8fe31365809391ba0b67ada05241d77c9baf953
David Oh <doh@thoughtworks.com>
2016-02-29 04:07:23 +0530

    David | #1663 | Removed legacy getPivotTables method from reportService now that it is no longer being used

--- src/main/js/app/service/report.service.js
+++ src/main/js/app/service/report.service.js
@@ -3,7 +3,6 @@ define(["dhisUrl", "lodash", "moment"], function(dhisUrl, _, moment) {
         var fieldAppReportRegex = /\[FieldApp - (.*)\]/;
 
         this.getReportDataForOrgUnit = function(report, orgUnit) {
-
             var buildDimension = function() {
                 var columnDimensions = _.map(report.columns, function(col) {
                     return col.dimension + ":" + _.pluck(col.items, "id").join(";");
@@ -43,79 +42,6 @@ define(["dhisUrl", "lodash", "moment"], function(dhisUrl, _, moment) {
                     "url": dhisUrl.analytics + "?" + queryString
                 });
             });
-        };
-
-        var filterAndMergeDatasetInfo = function(reports, datasets) {
-            var getDataset = function(report) {
-                var matches = fieldAppReportRegex.exec(report.name);
-                if (!matches || matches.length <= 1)
-                    return undefined;
-                var datasetCodeInReportName = matches[1];
-
-                return _.find(datasets, {
-                    'code': datasetCodeInReportName
-                });
-            };
-
-            return _.transform(reports, function(result, report) {
-
-                var dataset = getDataset(report);
-
-                if (dataset !== undefined) {
-                    result.push(_.merge(report, {
-                        'dataset': dataset.id
-                    }));
-                }
-
-            }, []);
-        };
-
-        var enrich = function(reports) {
-            var enrichReportPromises = _.map(reports, function(report) {
-                var url = report.href + "?fields=*,program[id,name],programStage[id,name],columns[dimension,filter,items[id,name]],rows[dimension,filter,items[id,name]],filters[dimension,filter,items[id,name]]";
-                return $http.get(url).then(function(response) {
-                    reportDetails = response.data;
-                    reportDetails.dataset = report.dataset;
-
-                    // TODO: Remove following three mappings after switching to DHIS 2.20 or greater
-                    reportDetails.rows = _.map(reportDetails.rows || [], function(row) {
-                        if (row.dimension === "in" || row.dimension === "de")
-                            row.dimension = "dx";
-                        return row;
-                    });
-                    reportDetails.columns = _.map(reportDetails.columns || [], function(column) {
-                        if (column.dimension === "in" || column.dimension === "de")
-                            column.dimension = "dx";
-                        return column;
-                    });
-                    reportDetails.filters = _.map(reportDetails.filters || [], function(filter) {
-                        if (filter.dimension === "in" || filter.dimension === "de")
-                            filter.dimension = "dx";
-                        return filter;
-                    });
-
-                    return reportDetails;
-                });
-            });
-            return $q.all(enrichReportPromises);
-        };
-
-        this.getPivotTables = function(datasets) {
-
-            var getFieldAppPivotTables = function() {
-                var config = {
-                    params: {
-                        "filter": "name:like:[FieldApp - ",
-                        "paging": false,
-                    }
-                };
-
-                return $http.get(dhisUrl.pivotTables, config).then(function(response) {
-                    return response.data ? filterAndMergeDatasetInfo(response.data.reportTables, datasets) : [];
-                });
-            };
-
-            return getFieldAppPivotTables().then(enrich);
         };
 
         var getResourceIds = function(resourceUrl, resourceCollectionName, lastUpdatedTime) {
