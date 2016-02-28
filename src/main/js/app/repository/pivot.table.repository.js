571f24d8365c9ed8a46b4ff86c9e6ed577ef05d8
David Oh <doh@thoughtworks.com>
2016-02-29 00:05:37 +0530

    David | #1663 | Refactored names of constants in pivotTableRepository

--- src/main/js/app/repository/pivot.table.repository.js
+++ src/main/js/app/repository/pivot.table.repository.js
@@ -1,17 +1,17 @@
 define(["lodash"], function(_) {
     return function(db) {
-        var pivotTableObjectStoreName = "pivotTables";
-        var pivotTableDataObjectStoreName = "pivotTableData";
+        var PIVOT_TABLE_STORE_NAME = 'pivotTables';
+        var PIVOT_TABLE_DATA_STORE_NAME = 'pivotTableData';
 
         this.replaceAll = function(pivotTables) {
-            var store = db.objectStore(pivotTableObjectStoreName);
+            var store = db.objectStore(PIVOT_TABLE_STORE_NAME);
             return store.clear().then(function() {
                 return store.upsert(pivotTables);
             });
         };
 
         this.upsertPivotTableData = function(pivotTableName, moduleId, data) {
-            var store = db.objectStore(pivotTableDataObjectStoreName);
+            var store = db.objectStore(PIVOT_TABLE_DATA_STORE_NAME);
             var pivotTableDataItem = {
                 pivotTable: pivotTableName,
                 orgUnit: moduleId,
@@ -30,13 +30,13 @@ define(["lodash"], function(_) {
         };
 
         this.getAll = function(pivotTables) {
-            var store = db.objectStore(pivotTableObjectStoreName);
+            var store = db.objectStore(PIVOT_TABLE_STORE_NAME);
             return store.getAll().then(addSortVars);
         };
 
         this.getDataForPivotTable = function(pivotTableName, orgUnitId) {
             var query = db.queryBuilder().$eq(pivotTableName).$index("by_pivot_table").compile();
-            var store = db.objectStore(pivotTableDataObjectStoreName);
+            var store = db.objectStore(PIVOT_TABLE_DATA_STORE_NAME);
 
             return store.each(query).then(function(data) {
                 var output = _(data).filter({
