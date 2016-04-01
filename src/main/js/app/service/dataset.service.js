d58f176802b62966ea7d3ba358f4198aa702081c
shital <shitalma@thoughtworks.com>
2016-04-01 11:39:38 +0530

    Jhansi, Shital | #1766 | Add request param field called ':identifiable' to dataset, organisationUnits and programs endpoints to make praxis compatible with dhis 2.22 upgrade

--- src/main/js/app/service/dataset.service.js
+++ src/main/js/app/service/dataset.service.js
@@ -5,7 +5,7 @@ define(["dhisUrl"], function(dhisUrl) {
             return $http.post(dhisUrl.dataSets + '/' + datasetId + '/organisationUnits/' + orgUnitId);
         };
         this.getAll = function(lastUpdatedTime) {
-            var url = dhisUrl.dataSets + '.json?fields=:all&paging=false';
+            var url = dhisUrl.dataSets + '.json?fields=:all,attributeValues[:identifiable,value,attribute[:identifiable]],organisationUnits[:identifiable]&paging=false';
             url = lastUpdatedTime ? url + "&filter=lastUpdated:gte:" + lastUpdatedTime : url;
             return $http.get(url).then(function(response) {
                 return response.data.dataSets;
--- src/test/unit/js/app/service/dataset.service.spec.js
+++ src/test/unit/js/app/service/dataset.service.spec.js
@@ -28,7 +28,7 @@ define(["datasetService", "angularMocks", "properties"], function(DatasetService
                 'dataSets': datasets
             };
 
-            httpBackend.expectGET(properties.dhis.url + "/api/dataSets.json?fields=:all&paging=false").respond(200, responsePayload);
+            httpBackend.expectGET(properties.dhis.url + "/api/dataSets.json?fields=:all,attributeValues[:identifiable,value,attribute[:identifiable]],organisationUnits[:identifiable]&paging=false").respond(200, responsePayload);
             httpBackend.flush();
 
             expect(actualDataSets).toEqual(responsePayload.dataSets);
@@ -42,7 +42,7 @@ define(["datasetService", "angularMocks", "properties"], function(DatasetService
 
             datasetService.getAll(lastUpdatedTime);
 
-            httpBackend.expectGET(properties.dhis.url + "/api/dataSets.json?fields=:all&paging=false&filter=lastUpdated:gte:" + lastUpdatedTime).respond(200, responsePayload);
+            httpBackend.expectGET(properties.dhis.url + "/api/dataSets.json?fields=:all,attributeValues[:identifiable,value,attribute[:identifiable]],organisationUnits[:identifiable]&paging=false&filter=lastUpdated:gte:" + lastUpdatedTime).respond(200, responsePayload);
             httpBackend.flush();
         });
 
