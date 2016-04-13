be9d607ccc5b4e0bed42b5b44efa07655d35027d
harikrishnakanchi <harikrishnakanchi@gmail.com>
2016-04-13 17:51:23 +0530

    Hari | #1768 | Added backward compatibility for associateorgUnitToDataset job

--- src/test/unit/js/app/consumers/update.dataset.consumer.spec.js
+++ src/test/unit/js/app/consumers/update.dataset.consumer.spec.js
@@ -1,6 +1,6 @@
-define(["updateDatasetConsumer", "utils", "angularMocks", "datasetService"], function(UpdateDatasetConsumer, utils, mocks, DatasetService) {
+define(["updateDatasetConsumer", "utils", "angularMocks", "datasetService", "datasetRepository"], function(UpdateDatasetConsumer, utils, mocks, DatasetService, DatasetRepository) {
     describe("updateDatasetConsumer", function() {
-        var updateDatasetConsumer, message, datasetService, q, scope;
+        var updateDatasetConsumer, message, datasetService, q, scope, datasetRepository;
 
         beforeEach(mocks.inject(function($q, $rootScope) {
             q = $q;
@@ -9,7 +9,9 @@ define(["updateDatasetConsumer", "utils", "angularMocks", "datasetService"], fun
             datasetService = new DatasetService();
             spyOn(datasetService, "assignOrgUnitToDataset").and.returnValue(utils.getPromise(q, {}));
 
-            updateDatasetConsumer = new UpdateDatasetConsumer(datasetService, q);
+            datasetRepository = new DatasetRepository();
+
+            updateDatasetConsumer = new UpdateDatasetConsumer(datasetService, q, datasetRepository);
         }));
 
         it("should save each orgUnit to each dataset", function() {
@@ -30,6 +32,29 @@ define(["updateDatasetConsumer", "utils", "angularMocks", "datasetService"], fun
             expect(datasetService.assignOrgUnitToDataset).toHaveBeenCalledWith('dataSetA', 'orgUnit2');
             expect(datasetService.assignOrgUnitToDataset).toHaveBeenCalledWith('dataSetB', 'orgUnit1');
             expect(datasetService.assignOrgUnitToDataset).toHaveBeenCalledWith('dataSetB', 'orgUnit2');
+        });
+
+        it("should save datasets to dhis", function() {
+            var allDatasets = [{
+                "id": "ds1",
+                "name": "Dataset1"
+            }, {
+                "id": "ds2",
+                "name": "Dataset2"
+            }];
+            spyOn(datasetService, "associateDataSetsToOrgUnit").and.returnValue(utils.getPromise(q, {}));
+            spyOn(datasetRepository, "findAllDhisDatasets").and.returnValue(utils.getPromise(q, allDatasets));
+
+            message = {
+                data: {
+                    data: ["ds1", "ds2"],
+                    type: "associateDataset"
+                }
+            };
+
+            updateDatasetConsumer.run(message);
+            scope.$apply();
+            expect(datasetService.associateDataSetsToOrgUnit).toHaveBeenCalledWith([allDatasets[0], allDatasets[1]]);
         });
     });
 });
