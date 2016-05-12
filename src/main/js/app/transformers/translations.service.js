1b62c81b818556768f76c0811f2db49f884ae239
jhansireddy <jhansireddy007@gmail.com>
2016-05-12 16:48:32 +0530

    Prachi, Shital, Jhansi| #1842 | Made Translation Service compatible with name and description properties

--- src/main/js/app/transformers/translations.service.js
+++ src/main/js/app/transformers/translations.service.js
@@ -1,6 +1,7 @@
 define([],function(){
     return function($q, db) {
         var translatableTypes = ["sections", "dataElements"];
+        var translatableProperties = ["name", "description"];
 
         var setLocale = function(locale){
             this.locale = locale;
@@ -21,18 +22,23 @@ define([],function(){
 
             return getTranslationsForLocale().then(function(data) {
                 var translationObject = _.filter(data, {objectId: objectToBeTranslated.id});
-                objectToBeTranslated.name = translationObject ? translationObject[0].value : objectToBeTranslated.name;
+                _.each(translatableProperties, function (property) {
+                    if(objectToBeTranslated[property]) {
+                        var translationsByProperty = _.filter(translationObject, {property: property});
+                        objectToBeTranslated[property] = translationsByProperty[0] ? translationsByProperty[0].value : objectToBeTranslated[property];
+                    }
+                });
 
                 _.find(objectToBeTranslated, function (value, key) {
-
-                    if(_.isArray(value) && _.contains(translatableTypes,key)){
+                    if(_.isArray(value) && _.contains(translatableTypes, key)){
                         return _.each(value,function(object){
                             return translate(object);
                         });
-                    } else if(_.isObject(value) && _.contains(translatableTypes,key)){
+                    } else if(_.isObject(value) && _.contains(translatableTypes, key)){
                         return translate(value);
                     }
                 });
+
                 return objectToBeTranslated;
             });
         };
--- src/test/unit/js/app/transformers/translations.service.spec.js
+++ src/test/unit/js/app/transformers/translations.service.spec.js
@@ -9,21 +9,35 @@ define(['translationsService', 'angularMocks', 'utils'], function (TranslationsS
 
             mockStore.each.and.callFake(function (query) {
                 var result = [{
-                    objectId: 'abc',
+                    objectId: 'id1',
                     value: 'frenchName',
-                    locale: 'fr'
+                    locale: 'fr',
+                    property: 'name'
                 }, {
-                    objectId: 'xyz',
+                    objectId: 'id2',
                     value: 'frenchSection',
-                    locale: 'fr'
+                    locale: 'fr',
+                    property: 'name'
                 }, {
-                    objectId: 'stu',
+                    objectId: 'id3',
                     value: 'frenchDataElement',
-                    locale: 'fr'
+                    locale: 'fr',
+                    property: 'name'
+                },{
+                    objectId: 'id4',
+                    value: 'french description',
+                    locale: 'fr',
+                    property: 'description'
+                },{
+                    objectId: 'id5',
+                    value: 'frenchDataElementDescription',
+                    locale: 'fr',
+                    property: 'description'
                 }, {
-                    objectId: 'ghi',
-                    value: 'frenchGHISection',
-                    locale: 'fr'
+                    objectId: 'id4',
+                    value: 'french name',
+                    locale: 'fr',
+                    property: 'name'
                 }];
                 return utils.getPromise(q, result);
             });
@@ -32,7 +46,7 @@ define(['translationsService', 'angularMocks', 'utils'], function (TranslationsS
         it('should translate name to french if locale is french', function () {
             var locale = 'fr';
             var obj = {
-                id: 'abc',
+                id: 'id1',
                 name: 'testName'
             };
 
@@ -47,7 +61,7 @@ define(['translationsService', 'angularMocks', 'utils'], function (TranslationsS
             scope.$apply();
 
             expect(actual).toEqual({
-                id: 'abc',
+                id: 'id1',
                 name: 'frenchName'
             });
         });
@@ -55,7 +69,7 @@ define(['translationsService', 'angularMocks', 'utils'], function (TranslationsS
         it('should not translate if locale is english', function () {
             var locale = 'en';
             var obj = {
-                id: 'abc',
+                id: 'id1',
                 name: 'testName'
             };
 
@@ -70,7 +84,7 @@ define(['translationsService', 'angularMocks', 'utils'], function (TranslationsS
             scope.$apply();
 
             expect(actual).toEqual({
-                id: 'abc',
+                id: 'id1',
                 name: 'testName'
             });
         });
@@ -78,20 +92,20 @@ define(['translationsService', 'angularMocks', 'utils'], function (TranslationsS
         it('should translate the name in the nested object structure if locale is anything other than english', function () {
             var locale = 'fr';
             var obj = {
-                id: 'abc',
+                id: 'id1',
                 name: 'testName',
                 sections: [{
-                    id: 'xyz',
+                    id: 'id2',
                     name: 'testSection',
                     dataElements: {
-                        id: 'stu',
+                        id: 'id3',
                         name: 'testDataElement'
                     }
                 }, {
-                    id: 'ghi',
+                    id: 'id4',
                     name: 'testSection',
                     dataElements: {
-                        id: 'stu',
+                        id: 'id3',
                         name: 'testDataElement'
                     }
                 }]
@@ -108,21 +122,145 @@ define(['translationsService', 'angularMocks', 'utils'], function (TranslationsS
             scope.$apply();
 
             expect(actual).toEqual({
-                id: 'abc',
+                id: 'id1',
                 name: 'frenchName',
                 sections: [{
-                    id: 'xyz',
+                    id: 'id2',
                     name: 'frenchSection',
                     dataElements: {
-                        id: 'stu',
+                        id: 'id3',
                         name: 'frenchDataElement'
                     }
                 }, {
-                    id: 'ghi',
-                    name: 'frenchGHISection',
+                    id: 'id4',
+                    name: 'french name',
                     dataElements: {
-                        id: 'stu',
+                        id: 'id3',
                         name: 'frenchDataElement'
+                    }
+                }]
+            });
+        });
+
+        it('should set the default english name if there was no translation with the selected locale', function () {
+            var locale = 'fr';
+            var obj = {
+                id: 'dhi',
+                name: 'testName'
+            };
+
+            translationsService = new TranslationsService(q, mockDB.db);
+            translationsService.setLocale(locale);
+
+            var actual;
+            translationsService.translate(obj).then(function (data) {
+                actual = data;
+            });
+
+            scope.$apply();
+
+            expect(actual).toEqual({
+                id: 'dhi',
+                name: 'testName'
+            });
+        });
+
+        it('should translate description property in the object with the selected locale', function () {
+            var locale = 'fr';
+            var obj = {
+                id: 'id4',
+                description: 'english description'
+            };
+
+            translationsService = new TranslationsService(q, mockDB.db);
+            translationsService.setLocale(locale);
+
+            var actual;
+            translationsService.translate(obj).then(function (data) {
+                actual = data;
+            });
+
+            scope.$apply();
+
+            expect(actual).toEqual({
+                id: 'id4',
+                description: 'french description'
+            });
+        });
+
+        it('should translate name and description properties in the object with the selected locale', function () {
+            var locale = 'fr';
+            var obj = {
+                id: 'id4',
+                name: 'english name',
+                description: 'english description'
+            };
+
+            translationsService = new TranslationsService(q, mockDB.db);
+            translationsService.setLocale(locale);
+
+            var actual;
+            translationsService.translate(obj).then(function (data) {
+                actual = data;
+            });
+
+            scope.$apply();
+
+            expect(actual).toEqual({
+                id: 'id4',
+                name: 'french name',
+                description: 'french description'
+            });
+        });
+
+        it('should translate the description in the nested object structure if locale is anything other than english', function () {
+            var locale = 'fr';
+            var obj = {
+                id: 'id1',
+                name: 'testName',
+                sections: [{
+                    id: 'id2',
+                    name: 'testSection',
+                    dataElements: {
+                        id: 'id3',
+                        name: 'testDataElement'
+                    }
+                }, {
+                    id: 'id4',
+                    name: 'testSection',
+                    dataElements: {
+                        id: 'id5',
+                        description: 'testDataElementDescription'
+                    }
+                }]
+            };
+
+            translationsService = new TranslationsService(q, mockDB.db);
+            translationsService.setLocale(locale);
+
+            var actual;
+            translationsService.translate(obj).then(function (data) {
+                actual = data;
+            });
+
+            scope.$apply();
+
+            expect(actual).toEqual({
+                id: 'id1',
+                name: 'frenchName',
+                sections: [{
+                    id: 'id2',
+                    name: 'frenchSection',
+                    dataElements: {
+                        id: 'id3',
+                        name: 'frenchDataElement'
+                    }
+                }, {
+                    id: 'id4',
+                    name: 'french name',
+                    dataElements: {
+                        id: 'id5',
+                        description: 'frenchDataElementDescription'
                     }
                 }]
             });
