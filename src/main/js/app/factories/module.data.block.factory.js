533919f9d8f83dd79c1d874923aa6908cf47a32b
David Oh <doh@thoughtworks.com>
2016-05-21 00:43:56 +0530

    David | #1632 | Refactored module data block factory

--- src/main/js/app/factories/module.data.block.factory.js
+++ src/main/js/app/factories/module.data.block.factory.js
@@ -2,65 +2,67 @@ define(['moduleDataBlock', 'lodash'], function (ModuleDataBlock, _) {
     return function ($q, orgUnitRepository, dataRepository, programEventRepository, approvalDataRepository) {
 
         var createForProject = function (projectId, periodRange) {
-            return orgUnitRepository.getAllModulesInOrgUnits(projectId).then(function (moduleOrgUnits) {
-                var moduleIds = _.pluck(moduleOrgUnits, 'id');
+            return orgUnitRepository.getAllModulesInOrgUnits(projectId).then(_.partial(createForModules, _, periodRange));
+        };
 
-                var getIndexedAggregateData = function () {
-                    return dataRepository.getDataValuesForOrgUnitsAndPeriods(moduleIds, periodRange).then(function (aggregateDataValues) {
-                        return _.indexBy(aggregateDataValues, function (dataValue) {
-                            return dataValue.period + dataValue.orgUnit;
-                        });
+        var createForModules = function (moduleOrgUnits, periodRange) {
+            var moduleIds = _.pluck(moduleOrgUnits, 'id');
+
+            var getIndexedAggregateData = function () {
+                return dataRepository.getDataValuesForOrgUnitsAndPeriods(moduleIds, periodRange).then(function (aggregateDataValues) {
+                    return _.indexBy(aggregateDataValues, function (dataValue) {
+                        return dataValue.period + dataValue.orgUnit;
                     });
-                };
-
-                var getIndexedLineListData = function() {
-                    return orgUnitRepository.findAllByParent(moduleIds).then(function (originOrgUnits) {
-                        var startPeriod = periodRange[0],
-                            originOrgUnitIds = _.pluck(originOrgUnits, 'id');
-
-                        var getModuleIdFromOriginOrgUnits = _.memoize(function(originOrgUnitId) {
-                            return _.find(originOrgUnits, { id: originOrgUnitId }).parent.id;
-                        });
-
-                        return programEventRepository.getEventsFromPeriod(startPeriod, originOrgUnitIds).then(function (lineListEvents) {
-                            return _.groupBy(lineListEvents, function(lineListEvent) {
-                               return lineListEvent.period + getModuleIdFromOriginOrgUnits(lineListEvent.orgUnit);
-                            });
-                        });
-                    });
-                };
-
-                var getIndexedApprovalData = function() {
-                    var startPeriod = periodRange[0],
-                        endPeriod = _.last(periodRange);
-                    return approvalDataRepository.getApprovalDataForPeriodsOrgUnits(startPeriod, endPeriod, moduleIds).then(function (allApprovalData) {
-                       allApprovalData = _.reject(allApprovalData, 'status', 'DELETED');
-                       return _.indexBy(allApprovalData, function (approvalData) {
-                           return approvalData.period + approvalData.orgUnit;
-                       });
-                    });
-                };
-
-                return $q.all({
-                    indexedAggregateData: getIndexedAggregateData(),
-                    indexedLineListData: getIndexedLineListData(),
-                    indexedApprovalData: getIndexedApprovalData()
-                }).then(function (data) {
-                    var indexedAggregateData = data.indexedAggregateData;
-                    var indexedLineListData = data.indexedLineListData;
-                    var indexedApprovalData = data.indexedApprovalData;
-
-                    var allModuleDataBlocks = _.map(moduleOrgUnits, function (moduleOrgUnit) {
-                        return _.map(periodRange, function (period) {
-                            var aggregateDataValues = indexedAggregateData[period + moduleOrgUnit.id] || {};
-                            var lineListData = indexedLineListData[period + moduleOrgUnit.id] || [];
-                            var approvalData = indexedApprovalData[period + moduleOrgUnit.id] || {};
-
-                            return ModuleDataBlock.create(moduleOrgUnit, period, aggregateDataValues, lineListData, approvalData);
-                        });
-                    });
-                    return _.flatten(allModuleDataBlocks);
                 });
+            };
+
+            var getIndexedLineListData = function() {
+                return orgUnitRepository.findAllByParent(moduleIds).then(function (originOrgUnits) {
+                    var startPeriod = periodRange[0],
+                        originOrgUnitIds = _.pluck(originOrgUnits, 'id');
+
+                    var getModuleIdFromOriginOrgUnits = _.memoize(function(originOrgUnitId) {
+                        return _.find(originOrgUnits, { id: originOrgUnitId }).parent.id;
+                    });
+
+                    return programEventRepository.getEventsFromPeriod(startPeriod, originOrgUnitIds).then(function (lineListEvents) {
+                        return _.groupBy(lineListEvents, function(lineListEvent) {
+                            return lineListEvent.period + getModuleIdFromOriginOrgUnits(lineListEvent.orgUnit);
+                        });
+                    });
+                });
+            };
+
+            var getIndexedApprovalData = function() {
+                var startPeriod = periodRange[0],
+                    endPeriod = _.last(periodRange);
+                return approvalDataRepository.getApprovalDataForPeriodsOrgUnits(startPeriod, endPeriod, moduleIds).then(function (allApprovalData) {
+                    allApprovalData = _.reject(allApprovalData, 'status', 'DELETED');
+                    return _.indexBy(allApprovalData, function (approvalData) {
+                        return approvalData.period + approvalData.orgUnit;
+                    });
+                });
+            };
+
+            return $q.all({
+                indexedAggregateData: getIndexedAggregateData(),
+                indexedLineListData: getIndexedLineListData(),
+                indexedApprovalData: getIndexedApprovalData()
+            }).then(function (data) {
+                var indexedAggregateData = data.indexedAggregateData;
+                var indexedLineListData = data.indexedLineListData;
+                var indexedApprovalData = data.indexedApprovalData;
+
+                var allModuleDataBlocks = _.map(moduleOrgUnits, function (moduleOrgUnit) {
+                    return _.map(periodRange, function (period) {
+                        var aggregateDataValues = indexedAggregateData[period + moduleOrgUnit.id] || {};
+                        var lineListData = indexedLineListData[period + moduleOrgUnit.id] || [];
+                        var approvalData = indexedApprovalData[period + moduleOrgUnit.id] || {};
+
+                        return ModuleDataBlock.create(moduleOrgUnit, period, aggregateDataValues, lineListData, approvalData);
+                    });
+                });
+                return _.flatten(allModuleDataBlocks);
             });
         };
 
