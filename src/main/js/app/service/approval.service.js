b4774fad3a897a5d2e3b3d69e6196eec5cf54f76
harikrishnakanchi <harikrishnakanchi@gmail.com>
2016-05-24 14:59:45 +0530

    Prachi, Hari | #1892 | Added compatability for DHIS 2.22 to check dataset is locked during completionData

--- src/main/js/app/service/approval.service.js
+++ src/main/js/app/service/approval.service.js
@@ -14,16 +14,18 @@ define(["properties", "moment", "dhisUrl", "lodash", "dateUtils"], function(prop
                     });
                 });
             });
+            var isAnyDataSetIsLocked = function (response) {
+                var responseData = _.isString(response.data) ? response.data : response.data.message;
+                if (response.status == 409 && _.contains(responseData, "Data set is locked:")) {
+                    return $q.when();
+                }
+                else {
+                    return $q.reject();
+                }
+            };
             // TODO: Remove catch block after 6.0 update
             return $http.post(dhisUrl.approvalMultipleL1, payload)
-                .catch(function (response) {
-                    if (response.status == 409 && _.contains(response.data, "Data set is locked:")) {
-                        return $q.when();
-                    }
-                    else {
-                        return $q.reject();
-                    }
-                });
+                .catch(isAnyDataSetIsLocked);
         };
 
         this.markAsApproved = function(dataSets, periodsAndOrgUnits, approvedBy, approvalDate) {
