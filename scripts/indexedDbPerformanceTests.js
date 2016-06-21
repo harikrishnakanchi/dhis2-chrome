088cd9d667fc30aa485361afa2f0d8f6e653b823
David Oh <doh@thoughtworks.com>
2016-06-21 12:10:40 +0530

    Added tests for using GetAll with queries and in batches

--- scripts/indexedDbPerformanceTests.js
+++ scripts/indexedDbPerformanceTests.js
@@ -155,7 +155,6 @@ var runTest = function(testNumber) {
     };
 
     var testFindAllWithCursorWithHalfArray = function(db) {
-        var parentIds = ["a0f9b7ceb46", "a4113d91a74", "a8549a1f334", "a87c87ab3aa", "a2b5312f50c", "a6e473b1bc9", "a92f69580e8", "ab31ea68dca", "a69cc45b4b4", "a8fcd3619d8", "a9437a0828c", "ad272fbcb70", "a329cd98589", "a6fbd65eccf", "a74f119e557", "abd19473a1d", "a24d00fde4a", "a3e5a22bcd0", "a8f472b5d40", "acd5b3f9503", "a2937f49aea", "a3103f88b25", "a4bf24606fe", "a4c1b6d17ac", "a4e705cc3c3", "a563a821021", "a5740b2e6c9", "a7db412f24f", "ab47122e0cd", "ae62ff677bb"];
         transaction = db.transaction(STORE_NAME, "readwrite");
         objectStore = transaction.objectStore(STORE_NAME);
 
@@ -193,8 +192,46 @@ var runTest = function(testNumber) {
         };
     };
 
+    var testFindAllWithCursorWithOneElement = function(db) {
+        transaction = db.transaction(STORE_NAME, "readwrite");
+        objectStore = transaction.objectStore(STORE_NAME);
+
+        console.time('testFindAllWithCursorWithOneElement');
+        req = objectStore.index('by_parent').openCursor(null, 'next');
+        var sortedParentIds = PARENT_IDS.sort().slice(0,1);
+        var orgUnits = [];
+        req.onsuccess = function(e) {
+            var foundAMatch = function() {
+                orgUnits.push(cursor.value);
+                cursor.continue();
+            };
+
+            var finishedQuery = function() {
+                console.timeEnd('testFindAllWithCursorWithOneElement');
+                console.log('orgUnits: ', orgUnits.length);
+            };
+
+            var cursor = e.target.result;
+            if (!cursor) {
+                finishedQuery();
+                return;
+            }
+
+            var i = 0;
+            var currentId = cursor.key;
+            while (currentId > sortedParentIds[i]) {
+                i++;
+                if (i === sortedParentIds.length) {
+                    finishedQuery();
+                    return;
+                }
+            }
+            currentId === sortedParentIds[i] ? foundAMatch() : cursor.continue(sortedParentIds[i]);
+        };
+    };
+
+
     var testFindAllWithCursorAndKeyRange = function(db) {
-        var parentIds = ["a0f9b7ceb46", "a4113d91a74", "a8549a1f334", "a87c87ab3aa", "a2b5312f50c", "a6e473b1bc9", "a92f69580e8", "ab31ea68dca", "a69cc45b4b4", "a8fcd3619d8", "a9437a0828c", "ad272fbcb70", "a329cd98589", "a6fbd65eccf", "a74f119e557", "abd19473a1d", "a24d00fde4a", "a3e5a22bcd0", "a8f472b5d40", "acd5b3f9503", "a2937f49aea", "a3103f88b25", "a4bf24606fe", "a4c1b6d17ac", "a4e705cc3c3", "a563a821021", "a5740b2e6c9", "a7db412f24f", "ab47122e0cd", "ae62ff677bb"];
         transaction = db.transaction(STORE_NAME, "readwrite");
         objectStore = transaction.objectStore(STORE_NAME);
 
@@ -274,15 +311,13 @@ var runTest = function(testNumber) {
             };
         };
 
-
-        var parentIds = ["a0f9b7ceb46", "a4113d91a74", "a8549a1f334", "a87c87ab3aa", "a2b5312f50c", "a6e473b1bc9", "a92f69580e8", "ab31ea68dca", "a69cc45b4b4", "a8fcd3619d8", "a9437a0828c", "ad272fbcb70", "a329cd98589", "a6fbd65eccf", "a74f119e557", "abd19473a1d", "a24d00fde4a", "a3e5a22bcd0", "a8f472b5d40", "acd5b3f9503", "a2937f49aea", "a3103f88b25", "a4bf24606fe", "a4c1b6d17ac", "a4e705cc3c3", "a563a821021", "a5740b2e6c9", "a7db412f24f", "ab47122e0cd", "ae62ff677bb"];
         transaction = db.transaction(STORE_NAME, "readonly");
         objectStore = transaction.objectStore(STORE_NAME);
 
         console.time('testFindAllWithCursorInBatches');
         var sortedParentIds = PARENT_IDS.sort();
 
-        var BATCH_SIZE = 3;
+        var BATCH_SIZE = window.batch_size || 3;
         for(i = 0; i < sortedParentIds.length; i += BATCH_SIZE) {
             var batchOfIds = sortedParentIds.slice(i, i + BATCH_SIZE);
             executeRequestForBatch(batchOfIds, function() {
@@ -292,19 +327,247 @@ var runTest = function(testNumber) {
         }
     };
 
+    var testFindAllKeysWithCursorInBatches = function(db) {
+        var orgUnits = [],
+            requestsToComplete = 0;
+
+        var executeRequestForBatch = function(batchOfIds, completedFn) {
+            console.log('Created batch for ids: ', batchOfIds.length, batchOfIds);
+            requestsToComplete++;
+            var thisRequest = objectStore.index('by_parent').openCursor(IDBKeyRange.lowerBound(batchOfIds[0]), 'next');
+            var lastMatchedKey = null;
+            thisRequest.onsuccess = function(e) {
+                var foundAMatch = function() {
+                    lastMatchedKey = cursor.key;
+                    orgUnits.push(cursor.key);
+                    cursor.continue();
+                };
+
+                var finishedQuery = function() {
+                    requestsToComplete--;
+                    if(requestsToComplete == 0) {
+                        completedFn();
+                    }
+                };
+
+                var cursor = e.target.result;
+                if (!cursor) {
+                    finishedQuery();
+                    return;
+                }
+
+                var j = 0;
+                var currentId = cursor.key;
+                while (currentId != lastMatchedKey && currentId > batchOfIds[j]) {
+                    j++;
+                    if (j === batchOfIds.length) {
+                        finishedQuery();
+                        return;
+                    }
+                }
+                (currentId === batchOfIds[j] || currentId === lastMatchedKey) ? foundAMatch() : cursor.continue(batchOfIds[j]);
+            };
+        };
+
+        transaction = db.transaction(STORE_NAME, "readonly");
+        objectStore = transaction.objectStore(STORE_NAME);
+
+        console.time('testFindAllKeysWithCursorInBatches');
+        var sortedParentIds = PARENT_IDS.sort();
+
+        var BATCH_SIZE = window.batch_size || 3;
+        for(i = 0; i < sortedParentIds.length; i += BATCH_SIZE) {
+            var batchOfIds = sortedParentIds.slice(i, i + BATCH_SIZE);
+            executeRequestForBatch(batchOfIds, function() {
+                console.timeEnd('testFindAllKeysWithCursorInBatches');
+                console.log('orgUnits: ', orgUnits.length);
+            });
+        }
+    };
+
+
+    var testFindAllKeysWithCursor = function(db) {
+        transaction = db.transaction(STORE_NAME, "readwrite");
+        objectStore = transaction.objectStore(STORE_NAME);
+
+        console.time('testFindAllKeysWithCursor');
+        req = objectStore.index('by_parent').openCursor(null, 'next');
+        var sortedParentIds = PARENT_IDS.sort();
+        var orgUnits = [];
+        req.onsuccess = function(e) {
+            var foundAMatch = function() {
+                orgUnits.push(cursor.key);
+                cursor.continue();
+            };
+
+            var finishedQuery = function() {
+                console.timeEnd('testFindAllKeysWithCursor');
+                console.log('orgUnits: ', orgUnits.length);
+            };
+
+            var cursor = e.target.result;
+            if (!cursor) {
+                finishedQuery();
+                return;
+            }
+
+            var i = 0;
+            var currentId = cursor.key;
+            while (currentId > sortedParentIds[i]) {
+                i++;
+                if (i === sortedParentIds.length) {
+                    finishedQuery();
+                    return;
+                }
+            }
+            currentId === sortedParentIds[i] ? foundAMatch() : cursor.continue(sortedParentIds[i]);
+        };
+    };
+
+    var testFindAllKeysWithKeyCursor = function(db) {
+        transaction = db.transaction(STORE_NAME, "readwrite");
+        objectStore = transaction.objectStore(STORE_NAME);
+
+        console.time('testFindAllKeysWithKeyCursor');
+        req = objectStore.index('by_parent').openKeyCursor(null, 'next');
+        var sortedParentIds = PARENT_IDS.sort();
+        var orgUnits = [];
+        req.onsuccess = function(e) {
+            var foundAMatch = function() {
+                orgUnits.push(cursor.key);
+                cursor.continue();
+            };
+
+            var finishedQuery = function() {
+                console.timeEnd('testFindAllKeysWithKeyCursor');
+                console.log('orgUnits: ', orgUnits.length);
+            };
+
+            var cursor = e.target.result;
+            if (!cursor) {
+                finishedQuery();
+                return;
+            }
+
+            var i = 0;
+            var currentId = cursor.key;
+            while (currentId > sortedParentIds[i]) {
+                i++;
+                if (i === sortedParentIds.length) {
+                    finishedQuery();
+                    return;
+                }
+            }
+            currentId === sortedParentIds[i] ? foundAMatch() : cursor.continue(sortedParentIds[i]);
+        };
+    };
+
+    var testFindAllWithGetAllAndQueryAndOneElement = function(db) {
+        transaction = db.transaction(STORE_NAME, "readwrite");
+        objectStore = transaction.objectStore(STORE_NAME);
+
+        console.time('testFindAllWithGetAllAndQueryAndOneElement');
+        var parentId = PARENT_IDS[0];
+        req = objectStore.index('by_parent').getAll(parentId);
+        req.onsuccess = function(e) {
+            var orgUnits = e.target.result;
+            console.timeEnd('testFindAllWithGetAllAndQueryAndOneElement');
+            console.log('orgUnits: ', orgUnits.length);
+        };
+    };
+
+    var testFindAllWithGetAllAndQueryInBatches = function(db) {
+        var orgUnits = [],
+            requestsToComplete = 0;
+
+        var executeRequestForBatch = function(batchOfIds, completedFn) {
+            console.log('Created batch for ids: ', batchOfIds.length, batchOfIds);
+            requestsToComplete++;
+            var thisRequest = objectStore.index('by_parent').getAll(batchOfIds[0]);
+
+            thisRequest.onsuccess = function(e) {
+                orgUnits.push(e.target.result);
+
+                requestsToComplete--;
+                if(requestsToComplete == 0) {
+                    completedFn();
+                }
+            };
+        };
+
+        transaction = db.transaction(STORE_NAME, "readonly");
+        objectStore = transaction.objectStore(STORE_NAME);
+
+        console.time('testFindAllWithGetAllAndQueryInBatches');
+        var sortedParentIds = PARENT_IDS.sort().slice(0, window.array_limit || 30);
+
+        var BATCH_SIZE = 1;
+        for(i = 0; i < sortedParentIds.length; i += BATCH_SIZE) {
+            var batchOfIds = sortedParentIds.slice(i, i + BATCH_SIZE);
+            executeRequestForBatch(batchOfIds, function() {
+                console.timeEnd('testFindAllWithGetAllAndQueryInBatches');
+                orgUnits = _.flatten(orgUnits);
+                console.log('orgUnits: ', orgUnits.length, orgUnits[0]);
+            });
+        }
+    };
+
+    var testFindAllKeysWithGetAllAndQueryInBatches = function(db) {
+        var orgUnits = [],
+            requestsToComplete = 0;
+
+        var executeRequestForBatch = function(batchOfIds, completedFn) {
+            console.log('Created batch for ids: ', batchOfIds.length, batchOfIds);
+            requestsToComplete++;
+            var thisRequest = objectStore.index('by_parent').getAllKeys(batchOfIds[0]);
+
+            thisRequest.onsuccess = function(e) {
+                orgUnits.push(e.target.result);
+
+                requestsToComplete--;
+                if(requestsToComplete == 0) {
+                    completedFn();
+                }
+            };
+        };
+
+        transaction = db.transaction(STORE_NAME, "readonly");
+        objectStore = transaction.objectStore(STORE_NAME);
+
+        console.time('testFindAllKeysWithGetAllAndQueryInBatches');
+        var sortedParentIds = PARENT_IDS.sort().slice(0, window.array_limit || 30);
+
+        var BATCH_SIZE = 1;
+        for(i = 0; i < sortedParentIds.length; i += BATCH_SIZE) {
+            var batchOfIds = sortedParentIds.slice(i, i + BATCH_SIZE);
+            executeRequestForBatch(batchOfIds, function() {
+                console.timeEnd('testFindAllKeysWithGetAllAndQueryInBatches');
+                orgUnits = _.flatten(orgUnits);
+                console.log('orgUnits: ', orgUnits.length, orgUnits[0]);
+            });
+        }
+    };
+
     var init = function() {
         openDb('msf', function(db) {
             var tests = [
-                testGetAll,
+                testGetAll, //0
                 testGetAllWithReadWriteTransaction,
                 testGetAllWithCursor,
                 testGet,
                 testGetWithCursorAndKeyRangeOnly,
-                testGetWithCursorAndKeyRangeBound,
+                testGetWithCursorAndKeyRangeBound, //5
                 testFindAllWithCursor,
                 testFindAllWithCursorWithHalfArray,
+                testFindAllWithCursorWithOneElement,
                 testFindAllWithCursorAndKeyRange,
-                testFindAllWithCursorInBatches
+                testFindAllWithCursorInBatches, //10
+                testFindAllKeysWithCursorInBatches,
+                testFindAllKeysWithCursor,
+                testFindAllKeysWithKeyCursor,
+                testFindAllWithGetAllAndQueryAndOneElement,
+                testFindAllWithGetAllAndQueryInBatches, //15
+                testFindAllKeysWithGetAllAndQueryInBatches
             ]
             tests[testNumber](db);
         });
