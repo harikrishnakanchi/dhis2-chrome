8e5bed19db8dbd8bbf2288091d539ca32eb1fe30
Raghavendra <raghavr@thoughtworks.com>
2016-07-21 11:08:17 +0530

    Raghavendra, David | #1823 | Reformatted dateUtils spec

--- src/test/unit/js/app/utils/date.utils.spec.js
+++ src/test/unit/js/app/utils/date.utils.spec.js
@@ -10,28 +10,34 @@ define(["dateUtils", "moment", "timecop"], function(dateUtils, moment, timecop) 
             Timecop.uninstall();
         });
 
-        it("should convert moment object to dhis week format", function() {
-            var actualResult = dateUtils.toDhisFormat(moment());
+        describe('toDHISFormat', function () {
+            it("should convert moment object to dhis week format", function() {
+                var actualResult = dateUtils.toDhisFormat(moment());
 
-            expect(actualResult).toEqual("2014W22");
+                expect(actualResult).toEqual("2014W22");
+            });
         });
 
-        it("should return max time", function() {
-            var timeStrings = ["2014-05-30T12:43:54.972Z", "2014-05-30T18:43:54.972Z", "2014-04-30T12:43:54.972Z"];
-            var actualResult = dateUtils.max(timeStrings);
+        describe('maxTime', function () {
+            it("should return max time", function() {
+                var timeStrings = ["2014-05-30T12:43:54.972Z", "2014-05-30T18:43:54.972Z", "2014-04-30T12:43:54.972Z"];
+                var actualResult = dateUtils.max(timeStrings);
 
-            expect(actualResult).toEqual(moment("2014-05-30T18:43:54.972Z"));
+                expect(actualResult).toEqual(moment("2014-05-30T18:43:54.972Z"));
+            });
+
+            it("should adjust for timezones and return max time", function() {
+                var timeStrings = ["2014-05-30T12:00:00.000Z", "2014-05-30T18:00:00.000+0530"];
+                var actualResult = dateUtils.max(timeStrings);
+                expect(actualResult).toEqual(moment("2014-05-30T18:00:00.000+0530"));
+            });
         });
 
-        it("should adjust for timezones and return max time", function() {
-            var timeStrings = ["2014-05-30T12:00:00.000Z", "2014-05-30T18:00:00.000+0530"];
-            var actualResult = dateUtils.max(timeStrings);
-            expect(actualResult).toEqual(moment("2014-05-30T18:00:00.000+0530"));
-        });
-
-        it("should subtract the given number of weeks from today's date", function() {
-            var actualResult = dateUtils.subtractWeeks(8);
-            expect(actualResult).toEqual("2014-04-04");
+        describe('subtractWeeks', function () {
+            it("should subtract the given number of weeks from today's date", function() {
+                var actualResult = dateUtils.subtractWeeks(8);
+                expect(actualResult).toEqual("2014-04-04");
+            });
         });
 
         describe("getPeriodRange", function() {
