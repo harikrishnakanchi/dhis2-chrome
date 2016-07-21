45eade96d242c7394c14e40b47bd97d14fcc3f3a
Raghavendra <raghavr@thoughtworks.com>
2016-07-21 11:11:26 +0530

    Raghavendra, David | #1823 | Reformatted quotation marks in dateUtils spec

--- src/test/unit/js/app/utils/date.utils.spec.js
+++ src/test/unit/js/app/utils/date.utils.spec.js
@@ -1,8 +1,8 @@
-define(["dateUtils", "moment", "timecop"], function(dateUtils, moment, timecop) {
-    describe("date utils", function() {
+define(['dateUtils', 'moment', 'timecop'], function(dateUtils, moment, timecop) {
+    describe('date utils', function() {
         beforeEach(function() {
             Timecop.install();
-            Timecop.freeze(new Date("2014-05-30T12:43:54.972Z"));
+            Timecop.freeze(new Date('2014-05-30T12:43:54.972Z'));
         });
 
         afterEach(function() {
@@ -11,48 +11,48 @@ define(["dateUtils", "moment", "timecop"], function(dateUtils, moment, timecop) 
         });
 
         describe('toDHISFormat', function () {
-            it("should convert moment object to dhis week format", function() {
+            it('should convert moment object to dhis week format', function() {
                 var actualResult = dateUtils.toDhisFormat(moment());
 
-                expect(actualResult).toEqual("2014W22");
+                expect(actualResult).toEqual('2014W22');
             });
         });
 
         describe('maxTime', function () {
-            it("should return max time", function() {
-                var timeStrings = ["2014-05-30T12:43:54.972Z", "2014-05-30T18:43:54.972Z", "2014-04-30T12:43:54.972Z"];
+            it('should return max time', function() {
+                var timeStrings = ['2014-05-30T12:43:54.972Z', '2014-05-30T18:43:54.972Z', '2014-04-30T12:43:54.972Z'];
                 var actualResult = dateUtils.max(timeStrings);
 
-                expect(actualResult).toEqual(moment("2014-05-30T18:43:54.972Z"));
+                expect(actualResult).toEqual(moment('2014-05-30T18:43:54.972Z'));
             });
 
-            it("should adjust for timezones and return max time", function() {
-                var timeStrings = ["2014-05-30T12:00:00.000Z", "2014-05-30T18:00:00.000+0530"];
+            it('should adjust for timezones and return max time', function() {
+                var timeStrings = ['2014-05-30T12:00:00.000Z', '2014-05-30T18:00:00.000+0530'];
                 var actualResult = dateUtils.max(timeStrings);
-                expect(actualResult).toEqual(moment("2014-05-30T18:00:00.000+0530"));
+                expect(actualResult).toEqual(moment('2014-05-30T18:00:00.000+0530'));
             });
         });
 
         describe('subtractWeeks', function () {
-            it("should subtract the given number of weeks from today's date", function() {
+            it('should subtract the given number of weeks from the current date', function() {
                 var actualResult = dateUtils.subtractWeeks(8);
-                expect(actualResult).toEqual("2014-04-04");
+                expect(actualResult).toEqual('2014-04-04');
             });
         });
 
-        describe("getPeriodRange", function() {
+        describe('getPeriodRange', function() {
             beforeEach(function() {
-                Timecop.freeze(new Date("2016-01-13T12:34:56.789Z"));
+                Timecop.freeze(new Date('2016-01-13T12:34:56.789Z'));
             });
 
-            it("should return period range for the current week", function() {
+            it('should return period range for the current week', function() {
                 var actualResult = dateUtils.getPeriodRange(1);
-                expect(actualResult).toEqual(["2016W02"]);
+                expect(actualResult).toEqual(['2016W02']);
             });
 
             it('should return period range for the last X weeks including the current week', function() {
                 var actualResult = dateUtils.getPeriodRange(3);
-                expect(actualResult).toEqual(["2015W53", "2016W01", "2016W02"]);
+                expect(actualResult).toEqual(['2015W53', '2016W01', '2016W02']);
             });
         });
     });
