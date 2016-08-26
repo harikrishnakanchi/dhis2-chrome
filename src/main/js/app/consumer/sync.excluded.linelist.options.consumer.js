0a19ae00e293a87cca4e8fca5f51fece080caf7c
harikrishnakanchi <harikrishnakanchi@gmail.com>
2016-08-26 15:43:16 +0530

    Poulami, Hari | #2069 | Modified excludedLinelistOptions consumer to merge data based on lastUpdated time

--- src/main/js/app/consumer/sync.excluded.linelist.options.consumer.js
+++ src/main/js/app/consumer/sync.excluded.linelist.options.consumer.js
@@ -1,12 +1,28 @@
-define(["lodash"], function (_) {
+define(["lodash", "moment"], function (_, moment) {
     return function ($q, excludedLineListOptionsRepository, dataStoreService) {
         this.run = function (message) {
             var moduleId = message.data.data;
             if (!moduleId) {
                 return $q.when();
             }
-            return excludedLineListOptionsRepository.get(moduleId)
-                .then(_.partial(dataStoreService.updateExcludedOptions, moduleId));
+            return $q.all([excludedLineListOptionsRepository.get(moduleId), dataStoreService.getLastUpdatedTimeForExcludedOptions(moduleId)])
+                .then(function (data) {
+                    var localExcludedLineListOptions = data[0];
+                    var lastUpdatedTimeOnLocal = moment(localExcludedLineListOptions.clientLastUpdated);
+                    var lastUpdatedTimeOnRemote = data[1];
+                    if (lastUpdatedTimeOnRemote) {
+                        lastUpdatedTimeOnRemote = moment(lastUpdatedTimeOnRemote);
+                        if (lastUpdatedTimeOnLocal.isAfter(lastUpdatedTimeOnRemote)) {
+                            return dataStoreService.updateExcludedOptions(moduleId, localExcludedLineListOptions);
+                        }
+                        else {
+                            return dataStoreService.getExcludedOptions(moduleId).then(excludedLineListOptionsRepository.upsert);
+                        }
+                    }
+                    else {
+                        return dataStoreService.createExcludedOptions(moduleId, localExcludedLineListOptions);
+                    }
+                });
         };
     };
 });
--- src/test/unit/js/app/consumers/sync.excluded.linelist.options.consumer.spec.js
+++ src/test/unit/js/app/consumers/sync.excluded.linelist.options.consumer.spec.js
@@ -1,4 +1,4 @@
-define(['syncExcludedLinelistOptionsConsumer', 'angularMocks', 'utils', 'excludedLineListOptionsRepository'], function (SyncExcludedLinelistOptionsConsumer, mocks, utils, ExcludedLineListOptionsRepository) {
+define(['syncExcludedLinelistOptionsConsumer', 'angularMocks', 'utils', 'excludedLineListOptionsRepository', 'dataStoreService'], function (SyncExcludedLinelistOptionsConsumer, mocks, utils, ExcludedLineListOptionsRepository, DataStoreService) {
     var syncExcludedLinelistOptionsConsumer, q, scope, message, moduleId, excludedLineListOptionsRepository, dataStoreService;
 
     describe('syncExcludedLinelistOptionsConsumer', function () {
@@ -10,10 +10,13 @@ define(['syncExcludedLinelistOptionsConsumer', 'angularMocks', 'utils', 'exclude
             };
             moduleId = "someModuleId";
             excludedLineListOptionsRepository = new ExcludedLineListOptionsRepository();
-            spyOn(excludedLineListOptionsRepository, "get").and.returnValue(utils.getPromise(q, undefined));
-            dataStoreService = {
-                updateExcludedOptions: jasmine.createSpy("getExcludedOptions").and.returnValue(utils.getPromise(q, undefined))
-            };
+            spyOn(excludedLineListOptionsRepository, "get").and.returnValue(utils.getPromise(q, {}));
+            spyOn(excludedLineListOptionsRepository, "upsert").and.returnValue(utils.getPromise(q, undefined));
+            dataStoreService = new DataStoreService();
+            spyOn(dataStoreService, 'updateExcludedOptions').and.returnValue(utils.getPromise(q, undefined));
+            spyOn(dataStoreService, 'getLastUpdatedTimeForExcludedOptions').and.returnValue(utils.getPromise(q, undefined));
+            spyOn(dataStoreService, 'getExcludedOptions').and.returnValue(utils.getPromise(q, undefined));
+            dataStoreService.createExcludedOptions = jasmine.createSpy("createExcludedOptions").and.returnValue(utils.getPromise(q, undefined));
             syncExcludedLinelistOptionsConsumer = new SyncExcludedLinelistOptionsConsumer(q, excludedLineListOptionsRepository, dataStoreService);
         }));
 
@@ -44,12 +47,44 @@ define(['syncExcludedLinelistOptionsConsumer', 'angularMocks', 'utils', 'exclude
             expect(excludedLineListOptionsRepository.get).not.toHaveBeenCalled();
         });
 
-        it('should upload current excluded linelistOptions to DHIS', function() {
-            var excludedLineListOptions = mockExcludedLineListOptions();
+        it('should update excludedLinelist options on remote if remoteData is older than local data', function () {
+            var excludedLineListOptions = mockExcludedLineListOptions({clientLastUpdated: "2016-05-19T00:00:00.000Z"});
+            dataStoreService.getLastUpdatedTimeForExcludedOptions.and.returnValue(utils.getPromise(q, "2016-05-18T00:00:00.000Z"));
             excludedLineListOptionsRepository.get.and.returnValue(utils.getPromise(q, excludedLineListOptions));
+
             initializeConsumer(moduleId);
 
             expect(dataStoreService.updateExcludedOptions).toHaveBeenCalledWith(moduleId, excludedLineListOptions);
+        });
+
+        it('should get lastUpdated time on DHIS', function() {
+            initializeConsumer(moduleId);
+
+            expect(dataStoreService.getLastUpdatedTimeForExcludedOptions).toHaveBeenCalledWith(moduleId);
+        });
+
+        it('should update excludedLinelist options on local if localData is older than remote data', function() {
+            var localExcludedLineListOptions = mockExcludedLineListOptions({clientLastUpdated: "2016-05-19T00:00:00.000Z"});
+            var remoteExcludedLineListOptions = mockExcludedLineListOptions({clientLastUpdated: "2016-05-20T00:00:00.000Z"});
+            dataStoreService.getLastUpdatedTimeForExcludedOptions.and.returnValue(utils.getPromise(q, "2016-05-20T00:00:00.000Z"));
+            dataStoreService.getExcludedOptions.and.returnValue(utils.getPromise(q, remoteExcludedLineListOptions));
+            excludedLineListOptionsRepository.get.and.returnValue(utils.getPromise(q, localExcludedLineListOptions));
+
+            initializeConsumer(moduleId);
+
+            expect(dataStoreService.updateExcludedOptions).not.toHaveBeenCalledWith(moduleId, localExcludedLineListOptions);
+            expect(dataStoreService.getExcludedOptions).toHaveBeenCalledWith(moduleId);
+            expect(excludedLineListOptionsRepository.upsert).toHaveBeenCalledWith(remoteExcludedLineListOptions);
+        });
+
+        it('should create excludedOptions on DHIS if there is no remote data', function () {
+            var localExcludedLineListOptions = mockExcludedLineListOptions({clientLastUpdated: "2016-05-19T00:00:00.000Z"});
+            dataStoreService.getLastUpdatedTimeForExcludedOptions.and.returnValue(utils.getPromise(q, undefined));
+            excludedLineListOptionsRepository.get.and.returnValue(utils.getPromise(q, localExcludedLineListOptions));
+
+            initializeConsumer(moduleId);
+
+            expect(dataStoreService.createExcludedOptions).toHaveBeenCalledWith(moduleId, localExcludedLineListOptions);
         });
     });
 });
