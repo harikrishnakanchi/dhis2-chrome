1f27f2bad463c96bbd1c75a7ee86b693e3eeaa79
Prachi <prachisr@thoughtworks.com>
2016-09-08 18:39:46 +0530

    David, Prachi | #2121 | Using the enrichWithParent method in moduleWeekSelectorController

--- src/main/js/app/controller/module.week.selector.controller.js
+++ src/main/js/app/controller/module.week.selector.controller.js
@@ -56,7 +56,9 @@ define(["lodash", "moment", "interpolate"],
 
             var setAvailableModules = function() {
                 if ($rootScope.currentUser && $rootScope.currentUser.selectedProject) {
-                    return orgUnitRepository.getAllModulesInOrgUnits($rootScope.currentUser.selectedProject.id).then(function(modules) {
+                    return orgUnitRepository.getAllModulesInOrgUnits($rootScope.currentUser.selectedProject.id)
+                        .then(orgUnitRepository.enrichWithParent)
+                        .then(function(modules) {
                         $scope.modules = getFilteredModulesWithDisplayNames(modules);
                     });
                 } else {
