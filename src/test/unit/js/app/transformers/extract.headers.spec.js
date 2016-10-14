38e419ba614f0cdfa48d749624915e9e1c0c3b5f
Poulami <poulami@thoughtworks.com>
2016-10-14 12:13:45 +0530

    Poulami, David | #2157 | Refactored extractHeaders spec

--- src/test/unit/js/app/transformers/extract.headers.spec.js
+++ src/test/unit/js/app/transformers/extract.headers.spec.js
@@ -1,280 +1,94 @@
-define(["extractHeaders", "lodash"], function(extractHeaders, _) {
+define(['extractHeaders', 'lodash'], function(extractHeaders, _) {
 
-    var categoryOptions = [],
-        categoryCombo;
-    beforeEach(function() {
-        categoryOptions = [{
-            "name": "Resident",
-            "id": "Resident"
-        }, {
-            "name": "Migrant",
-            "id": "Migrant"
-        }, {
-            "name": "LessThan5",
-            "id": "LessThan5"
-        }, {
-            "name": "GreaterThan5",
-            "id": "GreaterThan5"
-        }, {
-            "name": "New",
-            "id": "New"
-        }, {
-            "name": "FollowUp",
-            "id": "FollowUp"
-        }];
+    var categoryOption1, categoryOption2, categoryOption3, categoryOption4, categoryOption5, categoryOption6, categoryCombo, categories,
+        categoryOptionCombos, categoryOptionComboA, categoryOptionComboB, categoryOptionComboC, categoryOptionComboD;
+
+    beforeEach(function () {
+        categoryOption1 = {
+            id: 'categoryOptionId1',
+            name: 'categoryOptionName1'
+        };
+
+        categoryOption2 = {
+            id: 'categoryOptionId2',
+            name: 'categoryOptionName2'
+        };
+
+        categoryOption3 = {
+            id: 'categoryOptionId3',
+            name: 'categoryOptionName3'
+        };
+
+        categoryOption4 = {
+            id: 'categoryOptionId4',
+            name: 'categoryOptionName4'
+        };
+
+        categoryOption5 = {
+            id: 'categoryOptionId5',
+            name: 'categoryOptionName5'
+        };
+
+        categoryOption6 = {
+            id: 'categoryOptionId6',
+            name: 'categoryOptionName6'
+        };
 
         categoryCombo = {
-            "id": "CC1"
+            "id": "someCategoryComboId"
         };
+
+        categories = [{
+            id: 'categoryId1',
+            categoryOptions: [categoryOption1]
+        }, {
+            id: 'categoryId2',
+            categoryOptions: [categoryOption2, categoryOption3]
+        }, {
+            id: 'categoryId3',
+            categoryOptions: [categoryOption4, categoryOption5]
+        }];
+
+        categoryOptionComboA = {
+            id: 'categoryOptionComboIdA',
+            categoryCombo: categoryCombo,
+            categoryOptions: [categoryOption1, categoryOption2, categoryOption4]
+        };
+        categoryOptionComboB = {
+            id: 'categoryOptionComboIdB',
+            categoryCombo: categoryCombo,
+            categoryOptions: [categoryOption1, categoryOption2, categoryOption5]
+        };
+        categoryOptionComboC = {
+            id: 'categoryOptionComboIdC',
+            categoryCombo: categoryCombo,
+            categoryOptions: [categoryOption1, categoryOption3, categoryOption4]
+        };
+        categoryOptionComboD = {
+            id: 'categoryOptionComboIdD',
+            categoryCombo: categoryCombo,
+            categoryOptions: [categoryOption1, categoryOption3, categoryOption5]
+        };
+        categoryOptionCombos = [categoryOptionComboA, categoryOptionComboB, categoryOptionComboC, categoryOptionComboD];
     });
 
-    describe("extract headers from category option combos", function() {
-        it("should extract headers 2 X 1", function() {
-
-            var categories = [{
-                id: 'cat1',
-                categoryOptions: [categoryOptions[0], categoryOptions[1]]
-            }];
-
-            var categoryOptionCombos = [{
-                "id": 1,
-                "name": "(CO1)",
-                "categoryCombo": {
-                    "id": "CC1"
-                },
-                "categoryOptions": [categoryOptions[0]]
-            }, {
-                "id": 2,
-                "name": "(CO2)",
-                "categoryCombo": {
-                    "id": "CC1"
-                },
-                "categoryOptions": [categoryOptions[1]]
-            }];
-
+    describe('headers', function() {
+        it('should extract headers 1 X 2 X 3', function() {
             var result = extractHeaders(categories, categoryCombo, categoryOptionCombos);
 
             expect(result.headers).toEqual([
-                [categoryOptions[0], categoryOptions[1]]
+                [categoryOption1],
+                [categoryOption2, categoryOption3],
+                [categoryOption4, categoryOption5, categoryOption4, categoryOption5]
             ]);
-            expect(result.categoryOptionComboIds).toEqual([1, 2]);
         });
+    });
 
-        it("should extract headers 2 X 2", function() {
-            var categories = [{
-                id: 'cat1',
-                categoryOptions: [categoryOptions[0], categoryOptions[1]]
-            }, {
-                id: 'cat1',
-                categoryOptions: [categoryOptions[2], categoryOptions[3]]
-            }];
-
-            var categoryOptionCombos = [{
-                "id": 1,
-                "categoryCombo": {
-                    "id": "CC1"
-                },
-                "name": "(CO1, CO3)",
-                "categoryOptions": [categoryOptions[0], categoryOptions[2]]
-            }, {
-                "id": 2,
-                "categoryCombo": {
-                    "id": "CC1"
-                },
-                "name": "(Resident, CO4)",
-                "categoryOptions": [categoryOptions[0], categoryOptions[3]]
-            }, {
-                "id": 3,
-                "categoryCombo": {
-                    "id": "CC1"
-                },
-                "name": "(CO2, LessThan5)",
-                "categoryOptions": [categoryOptions[2], categoryOptions[1]]
-            }, {
-                "id": 4,
-                "categoryCombo": {
-                    "id": "CC1"
-                },
-                "name": "(CO2, CO4)",
-                "categoryOptions": [categoryOptions[1], categoryOptions[3]]
-            }];
-
+    describe('categoryOptionComboIds', function () {
+        it('should extract categoryOptionComboIds', function () {
             var result = extractHeaders(categories, categoryCombo, categoryOptionCombos);
 
-            expect(result.headers).toEqual(
-                [
-                    [categoryOptions[0], categoryOptions[1]],
-                    [categoryOptions[2], categoryOptions[3], categoryOptions[2], categoryOptions[3]]
-                ]
-            );
-            expect(result.categoryOptionComboIds).toEqual([1, 2, 3, 4]);
-        });
-
-        it("should extract headers 1 X 2 X 3", function() {
-
-            var categories = [{
-                id: 'cat1',
-                categoryOptions: [{
-                    "name": "1",
-                    "id": "1"
-                }]
-            }, {
-                id: 'cat2',
-                categoryOptions: [{
-                    "name": "a",
-                    "id": "a"
-                }, {
-                    "name": "b",
-                    "id": "b"
-                }]
-            }, {
-                id: 'cat3',
-                categoryOptions: [{
-                    "name": "x",
-                    "id": "x"
-                }, {
-                    "name": "y",
-                    "id": "y"
-                }, {
-                    "name": "z",
-                    "id": "z"
-                }]
-            }];
-
-            var categoryOptionCombos = [{
-                "id": 1,
-                "categoryCombo": {
-                    "id": "CC1"
-                },
-                "name": "(1,a,x)",
-                "categoryOptions": [{
-                    "name": "1",
-                    "id": "1"
-                }, {
-                    "name": "a",
-                    "id": "a"
-                }, {
-                    "name": "x",
-                    "id": "x"
-                }]
-            }, {
-                "id": 2,
-                "categoryCombo": {
-                    "id": "CC1"
-                },
-                "name": "(1, a, y)",
-                "categoryOptions": [{
-                    "name": "1",
-                    "id": "1"
-                }, {
-                    "name": "a",
-                    "id": "a"
-                }, {
-                    "name": "y",
-                    "id": "y"
-                }]
-            }, {
-                "id": 3,
-                "categoryCombo": {
-                    "id": "CC1"
-                },
-                "name": "(1, a, z)",
-                "categoryOptions": [{
-                    "name": "1",
-                    "id": "1"
-                }, {
-                    "name": "a",
-                    "id": "a"
-                }, {
-                    "name": "z",
-                    "id": "z"
-                }]
-            }, {
-                "id": 4,
-                "categoryCombo": {
-                    "id": "CC1"
-                },
-                "name": "(1,b,x)",
-                "categoryOptions": [{
-                    "name": "1",
-                    "id": "1"
-                }, {
-                    "name": "b",
-                    "id": "b"
-                }, {
-                    "name": "x",
-                    "id": "x"
-                }]
-            }, {
-                "id": 5,
-                "categoryCombo": {
-                    "id": "CC1"
-                },
-                "name": "(1, b, y)",
-                "categoryOptions": [{
-                    "name": "b",
-                    "id": "b"
-                }, {
-                    "name": "y",
-                    "id": "y"
-                }, {
-                    "name": "1",
-                    "id": "1"
-                }]
-            }, {
-                "id": 6,
-                "categoryCombo": {
-                    "id": "CC1"
-                },
-                "name": "(1, b, z)",
-                "categoryOptions": [{
-                    "name": "1",
-                    "id": "1"
-                }, {
-                    "name": "b",
-                    "id": "b"
-                }, {
-                    "name": "z",
-                    "id": "z"
-                }]
-            }];
-
-            var result = extractHeaders(categories, categoryCombo, categoryOptionCombos);
-
-            expect(result.headers).toEqual([
-                [{
-                    "name": "1",
-                    "id": "1"
-                }],
-                [{
-                    "name": "a",
-                    "id": "a"
-                }, {
-                    "name": "b",
-                    "id": "b"
-                }],
-                [{
-                    "name": "x",
-                    "id": "x"
-                }, {
-                    "name": "y",
-                    "id": "y"
-                }, {
-                    "name": "z",
-                    "id": "z"
-                }, {
-                    "name": "x",
-                    "id": "x"
-                }, {
-                    "name": "y",
-                    "id": "y"
-                }, {
-                    "name": "z",
-                    "id": "z"
-                }]
-            ]);
-
-            expect(result.categoryOptionComboIds).toEqual([1, 2, 3, 4, 5, 6]);
+            expect(result.categoryOptionComboIds).toEqual([categoryOptionComboA.id, categoryOptionComboB.id, categoryOptionComboC.id, categoryOptionComboD.id]);
         });
     });
 });
