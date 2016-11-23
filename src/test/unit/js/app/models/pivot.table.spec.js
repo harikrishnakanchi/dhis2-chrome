1edccaf88bed9587a7aded185ffadd1dff37d487
Poulami <poulami@thoughtworks.com>
2016-11-23 10:58:03 +0530

    Poulami | #2113 | Added the opUnitReport indicator in pivotTable

--- src/test/unit/js/app/models/pivot.table.spec.js
+++ src/test/unit/js/app/models/pivot.table.spec.js
@@ -70,6 +70,18 @@ define(['pivotTable'], function(PivotTable) {
            });
        });
 
+       describe('opunitReport', function() {
+           it('should return true if pivot table name contains OpunitReport', function() {
+               pivotTable = PivotTable.create({ name: '[Praxis - OpunitReport] # Name' });
+               expect(pivotTable.opUnitReport).toBeTruthy();
+           });
+
+           it('should return false if pivot table name does not contain OpunitReport', function() {
+               pivotTable = PivotTable.create({ name: 'some malformed pivot table name' });
+               expect(pivotTable.opUnitReport).toBeFalsy();
+           });
+       });
+
        describe('geographicOriginReport', function() {
            it('should return true if pivot table name contains GeographicOrigin', function() {
                pivotTable = PivotTable.create({ name: '[FieldApp - GeographicOrigin] # Name' });
