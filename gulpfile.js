8e93840a7b834621f56dc71b3b44206486a93ada
Raghavendra <raghavr@thoughtworks.com>
2016-11-26 02:15:36 +0530

    Raghavendra | #000 | Adding generate-service-worker 'watch' task to 'serve' task

--- gulpfile.js
+++ gulpfile.js
@@ -73,7 +73,7 @@ gulp.task('update-webdriver', shell.task([
     './node_modules/protractor/bin/webdriver-manager update'
 ]));
 
-gulp.task('serve', ['generate-service-worker'], function() {
+gulp.task('serve', ['generate-service-worker', 'watch'], function() {
     webserver = http.createServer(
         ecstatic({
             root: __dirname + '/src/main',
