711554b42847f222c64ae334b63b49e1a8613761
Poulami <poulami@thoughtworks.com>
2016-12-02 10:41:26 +0530

    Revert "Poulami | #2256 | Not making calls if max number of retries is exceeded"
    This reverts commit 9a5ff5729d91c0af0fbf5728afe52696c0f63720.

--- src/main/js/app/interceptors/handle.timeout.interceptor.js
+++ src/main/js/app/interceptors/handle.timeout.interceptor.js
@@ -3,19 +3,12 @@ define(["properties", "platformUtils"], function(properties, platformUtils) {
         return {
             'responseError': function(response) {
                 var dhisMonitor = $injector.get('dhisMonitor');
-                if(!dhisMonitor.isOnline() && dhisMonitor.maxTriesReachedForNoNetwork()) {
-                    return $q.reject(response);
-                }
-
-                if (response.config.method === "GET" && (response.status === 0 || response.status === 504) &&
-                    (!response.config.params || !response.config.params.retry || response.config.params.retry < properties.queue.maxretries)) {
+                if (dhisMonitor.isOnline() && response.config.method === "GET" && (response.status === 0 || response.status === 504)) {
                     platformUtils.sendMessage("timeoutOccurred");
                     var $http = $injector.get('$http');
                     response.config.params = response.config.params || {};
                     response.config.params.retry = response.config.params.retry || 0;
                     response.config.params.retry++;
-                    if(!dhisMonitor.isOnline() && response.config.params.retry == properties.queue.maxretries)
-                        dhisMonitor.setMaxTriesIfNoNetwork();
                     return $timeout(function() {
                         return $http(response.config);
                     }, properties.queue.httpGetRetryDelay);
--- src/test/unit/js/app/interceptors/handle.timeout.interceptor.spec.js
+++ src/test/unit/js/app/interceptors/handle.timeout.interceptor.spec.js
@@ -15,30 +15,53 @@ define(["handleTimeoutInterceptor", "angularMocks", "properties", "platformUtils
             var injectorGetStubs = {
                 '$http': fakeHttp,
                 'dhisMonitor': {
-                    isOnline: jasmine.createSpy('dhisMonitorIsOnline').and.returnValue(true),
-                    maxTriesReachedForNoNetwork: jasmine.createSpy('maxTriesReachedForNoNetwork').and.returnValue(false)
+                    'isOnline': jasmine.createSpy('dhisMonitorIsOnline').and.returnValue(true)
                 }
             };
 
             injector.get.and.callFake(function(service) {
                 return injectorGetStubs[service];
             });
-            spyOn(q, "reject");
-            spyOn(platformUtils, "sendMessage");
-
-            properties.queue.maxretries = 5;
 
             handleTimeoutInterceptor = new HandleTimeoutInterceptor(q, injector, timeout);
         }));
 
-        it("should not retry in case of non-GET requests", function() {
+        it("should retry in case of timeout", function() {
+            spyOn(q, "reject");
+            spyOn(platformUtils, "sendMessage");
+
             var rejection = {
-                config: {
-                    method: "HEAD",
-                    url: properties.dhisPing.url + "?sdfgdsfgsdfg",
-                    params: {
-                        retry: 0
-                    }
+                "config": {
+                    "method": "GET",
+                    "url": "http://localhost:8080/api/someUrl"
+                },
+                "status": 0
+            };
+
+            handleTimeoutInterceptor.responseError(rejection);
+
+            expect(q.reject).not.toHaveBeenCalled();
+            expect(platformUtils.sendMessage).toHaveBeenCalledWith("timeoutOccurred");
+
+            timeout.flush();
+
+            expect(fakeHttp).toHaveBeenCalledWith({
+                method: 'GET',
+                url: 'http://localhost:8080/api/someUrl',
+                params: {
+                    retry: 1
+                }
+            });
+        });
+
+        it("should not retry in case of non-GET requests", function() {
+            spyOn(q, "reject");
+            spyOn(platformUtils, "sendMessage");
+
+            var rejection = {
+                "config": {
+                    "method": "HEAD",
+                    "url": properties.dhisPing.url + "?sdfgdsfgsdfg"
                 },
                 "status": 0
             };
@@ -48,48 +71,6 @@ define(["handleTimeoutInterceptor", "angularMocks", "properties", "platformUtils
             expect(q.reject).toHaveBeenCalledWith(rejection);
             expect(platformUtils.sendMessage).not.toHaveBeenCalled();
             expect(fakeHttp).not.toHaveBeenCalled();
-        });
-
-        describe('should retry', function () {
-            var rejection;
-            beforeEach(function () {
-                rejection = {
-                    "config": {
-                        "method": "GET",
-                        "url": "http://localhost:8080/api/someUrl",
-                        params: {
-                            retry: 0
-                        }
-                    },
-                    "status": 0
-                };
-            });
-
-            it("should retry in case of timeout", function() {
-                handleTimeoutInterceptor.responseError(rejection);
-
-                expect(q.reject).not.toHaveBeenCalled();
-                expect(platformUtils.sendMessage).toHaveBeenCalledWith("timeoutOccurred");
-
-                timeout.flush();
-
-                expect(fakeHttp).toHaveBeenCalledWith({
-                    method: 'GET',
-                    url: 'http://localhost:8080/api/someUrl',
-                    params: {
-                        retry: 1
-                    }
-                });
-            });
-
-            it('should retry for only max number of retries', function () {
-                rejection.config.params.retry = 6;
-                handleTimeoutInterceptor.responseError(rejection);
-
-                expect(q.reject).toHaveBeenCalled();
-                expect(fakeHttp).not.toHaveBeenCalled();
-            });
-
         });
     });
 });
