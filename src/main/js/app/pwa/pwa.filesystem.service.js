ff85cafc196b2ae1d669bd7aab3a68bdaff369e1
jhansireddy <jhansireddy007@gmail.com>
2016-12-12 19:05:22 +0530

    Temp commit

--- src/main/js/app/pwa/pwa.filesystem.service.js
+++ src/main/js/app/pwa/pwa.filesystem.service.js
@@ -31,15 +31,48 @@ define(['fileSaver', 'interpolate'], function (saveAs, interpolate) {
            saveAs(contents, fileName);
        };
 
-       var fakeFunction = function () {
+       var readFile = function () {
+           var deferred = $q.defer();
 
+           var prepareImportFileDomElement = function () {
+               var createImportFileElement = function () {
+                   var domElement = document.createElement('input');
+                   domElement.id = 'importClone';
+                   domElement.accept = '.msf';
+                   domElement.type='file';
+                   return domElement;
+               };
+               var importAppCloneElement = createImportFileElement();
+               angular.element(document.body).append(importAppCloneElement);
+               importAppCloneElement.addEventListener('change', onImportFileSelect);
+               importAppCloneElement.click();
+               return importAppCloneElement;
+           };
+
+           var importAppCloneElement = prepareImportFileDomElement();
+           var onImportFileSelect = function (e) {
+               importAppCloneElement.remove();
+               if(e.target.files)
+               {
+                   var file = e.target.files[0];
+                   var reader = new FileReader();
+                   reader.onerror = function (err) {
+                       deferred.reject(err);
+                   };
+                   reader.onloadend = function (data) {
+                       deferred.resolve(data);
+                   };
+                   reader.readAsArrayBuffer(file);
+               }
+           };
+           return deferred.promise;
        };
 
        return {
            "FILE_TYPE_OPTIONS": FILE_TYPE_OPTIONS,
            "promptAndWriteFile": writeFileAndNotify,
            "writeFile": writeFile,
-           "readFile": fakeFunction
+           "readFile": readFile
        };
    };
 });
