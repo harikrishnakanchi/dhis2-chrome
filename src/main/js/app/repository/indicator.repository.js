25eee122aca06efefa83124af03c0f8de1d2590d
jhansireddy <jhansireddy007@gmail.com>
2016-12-30 12:41:58 +0530

    Prachi, Jhansi | #2269 | Added the IndicatorRepository and enriching the indicators with details

---
+++ src/main/js/app/repository/indicator.repository.js
@@ -0,0 +1,18 @@
+define(['lodash'], function (_) {
+    return function (db) {
+        var store = db.objectStore('indicators');
+
+        this.getAll = function () {
+            return store.getAll();
+        };
+
+        this.enrichWithIndicatorDetails = function (indicators) {
+            return this.getAll().then(function (indicatorsFromStore) {
+                var indexedIndicatorsFromStore = _.indexBy(indicatorsFromStore, 'id');
+                return _.map(indicators, function (indicator) {
+                    return _.merge(indicator, _.pick(indexedIndicatorsFromStore[indicator.id], ['name', 'shortName', 'description', 'numerator', 'denominator']));
+                });
+            });
+        };
+    };
+});
