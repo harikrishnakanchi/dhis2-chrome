c18139de504cb7c1767442ae982fb1d2d1646518
harikrishnakanchi <harikrishnakanchi@gmail.com>
2017-02-13 17:32:09 +0530

    Poulami, Hari | #2274 | Removed obsolete method from optionSet Repository

--- src/main/js/app/repository/option.set.repository.js
+++ src/main/js/app/repository/option.set.repository.js
@@ -7,63 +7,6 @@ define(['lodash', 'optionSetTransformer'], function(_, optionSetTransformer) {
             return store.getAll();
         };
 
-        this.getOptionSetMapping = function(moduleId, rejectDisabled) {
-            var referralLocations;
-
-            var buildMapForReferralOptionSet = function(optionSet, optionMapping, optionSetMapping, options) {
-                options = _.filter(optionSet.options, function(ops) {
-                    if (!rejectDisabled)
-                        return !_.isUndefined(referralLocations[ops.name]);
-                    else
-                        return !_.isUndefined(referralLocations[ops.name]) && !referralLocations[ops.name].isDisabled;
-                });
-                _.each(options, function(o) {
-                    o.displayName = referralLocations[o.name].name;
-                    optionMapping[o.id] = referralLocations[o.name].name;
-                });
-                optionSetMapping[optionSet.id] = _.map(options, function(option) {
-                    option.isDisabled = referralLocations[option.name].isDisabled;
-                    option.name = referralLocations[option.name].name;
-                    return option;
-                });
-                optionSetMapping[optionSet.id] = _.sortBy(options, 'name');
-            };
-
-            var createMaps = function() {
-                var store = db.objectStore("optionSets");
-                return store.getAll().then(function(optionSets) {
-                    var optionSetMapping = {};
-                    var optionMapping = {};
-                    _.forEach(optionSets, function(optionSet) {
-                        var options;
-                        if (_.endsWith(optionSet.code, "_referralLocations") && !_.isUndefined(referralLocations)) {
-                            buildMapForReferralOptionSet(optionSet, optionMapping, optionSetMapping, options);
-                        } else if (_.endsWith(optionSet.code, "_referralLocations") && _.isUndefined(referralLocations)) {
-                            optionSetMapping[optionSet.id] = undefined;
-                        } else {
-                            options = _.compact(optionSet.options);
-                            _.each(options, function(o) {
-                                optionMapping[o.id] = o.name;
-                            });
-                            optionSetMapping[optionSet.id] = _.sortBy(options, 'name');
-                        }
-
-                    });
-
-                    return {
-                        'optionSetMap': optionSetMapping,
-                        'optionMap': optionMapping
-                    };
-                });
-            };
-
-            return referralLocationsRepository.get(moduleId).then(function(data) {
-                referralLocations = data;
-                return createMaps();
-            });
-        };
-
-
         this.getOptionSets = function(moduleId) {
             var getAllParameters = function () {
                 return $q.all({
--- src/test/unit/js/app/repository/option.set.repository.spec.js
+++ src/test/unit/js/app/repository/option.set.repository.spec.js
@@ -46,64 +46,6 @@ define(["optionSetRepository", "angularMocks", "utils", "referralLocationsReposi
             expect(result).toEqual(allOptionSets);
         });
 
-        it("should get option set mapping", function() {
-            var allOptionSets = [{
-                'id': 'os1',
-                'options': [{
-                    'id': 'os1o1',
-                    'name': 'os1o1 name'
-                }]
-            }, {
-                'id': 'os2',
-                'options': [{
-                    'id': 'os2o1',
-                    'name': 'os2o1 name'
-                }]
-            }, {
-                'id': 'os3',
-                'code': '_referralLocations',
-                'options': [{
-                    'id': 'os3o1',
-                    'name': 'MSF Facility 1'
-                }]
-            }];
-
-            mockStore.getAll.and.returnValue(utils.getPromise(q, allOptionSets));
-
-
-            var optionSetMap, optionMap;
-
-            optionSetRepository.getOptionSetMapping().then(function(data) {
-                optionSetMap = data.optionSetMap;
-                optionMap = data.optionMap;
-            });
-            scope.$apply();
-
-            expect(optionSetMap).toEqual({
-                "os1": [{
-                    "id": 'os1o1',
-                    "name": 'os1o1 name'
-                }],
-                "os2": [{
-                    "id": 'os2o1',
-                    "name": 'os2o1 name'
-                }],
-                "os3": [{
-                    "id": 'os3o1',
-                    "name": 'Referral 1',
-                    "displayName": 'Referral 1',
-                    "isDisabled": false
-                }]
-            });
-            expect(optionMap).toEqual({
-                "os1o1": "os1o1 name",
-                "os2o1": "os2o1 name",
-                "os3o1": "Referral 1"
-            });
-
-
-        });
-
         it('should filter the optionSets by code', function () {
             var filteredOptionSet,
                 mockOptionSets = [{
