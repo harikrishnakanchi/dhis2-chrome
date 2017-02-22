cef686fb7dbb35532389f2ae1a6daaf1f4b77231
harikrishnakanchi <harikrishnakanchi@gmail.com>
2017-02-22 19:42:41 +0530

    Sayan, Hari | #2321 | Fix to bootstrap the foreground app only once

--- src/main/js/app/pwa/pwa.app.bootstrap.js
+++ src/main/js/app/pwa/pwa.app.bootstrap.js
@@ -6,15 +6,16 @@ require.config({
 self.basePath = "./";
 
 require(["app/pwa/pwa.app.config", "app/shared.app.config"], function() {
-    require(["app/app", "platformUtils", "moment"], function(app, platformUtils, moment) {
+    require(["app/app", "platformUtils", "moment", "lodash"], function(app, platformUtils, moment, _) {
         window.moment = moment;
+
         var initializeForeground = function () {
             app.bootstrap(app.init());
         };
 
         self.worker = new Worker('js/app/pwa/pwa.bg.bootstrap.js');
         platformUtils.init();
-        platformUtils.addListener("backgroundReady", initializeForeground);
+        platformUtils.addListener("backgroundReady", _.once(initializeForeground));
 
         window.addEventListener('online', function () {
             platformUtils.sendMessage('online');
