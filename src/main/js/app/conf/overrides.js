291901d85b9bdeb6ff94951dcae96452947a78fd
Madhoolika <mbulusu@thoughtworks.com>
2017-03-01 14:35:21 +0530

    Madhoolika, Raghavendra | #1995 | Added new praxis configurations for userName validations

--- src/main/js/app/conf/overrides.js
+++ src/main/js/app/conf/overrides.js
@@ -4,6 +4,16 @@ define([], function() {
             authHeader: 'Basic YWRtaW46ZGlzdHJpY3Q=',
             productKeyLevel: 'global'
         },
-        support_email: 'supportEmail'
+        support_email: 'supportEmail',
+        organisationSettings: {
+            geographicOriginDisabled: false,
+            referralLocationDisabled: false,
+            userNameValidations: {
+                "Data entry user": "PROJECT_CODE_PREFIX",
+                "Project Level Approver": "PROJECT_CODE_PREFIX",
+                "Observer": "PROJECT_CODE_PREFIX",
+                "Coordination Level Approver": "EMAIL"
+            }
+        }
     };
 });
