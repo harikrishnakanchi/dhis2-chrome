8ea7ee61af448cce11f1e95103f67eaa026b41e0
Prachi <prachisr@thoughtworks.com>
2017-04-20 18:34:37 +0530

    Madhoolika, Prachi | #1996 | Added groupSetLevel attribute to customAttributes

--- src/main/js/app/models/custom.attributes.js
+++ src/main/js/app/models/custom.attributes.js
@@ -34,7 +34,8 @@ define(['lodash', 'moment'], function(_, moment) {
         REASON_FOR_INTERVENTION_CODE: 'reasonForIntervention',
         MODE_OF_OPERATION_CODE: 'modeOfOperation',
         MODEL_OF_MANAGEMENT_CODE: 'modelOfManagement',
-        PROJECT_TYPE_CODE: 'projectType'
+        PROJECT_TYPE_CODE: 'projectType',
+        ORG_UNIT_GROUP_SET_LEVEL: 'groupSetLevel'
     };
 
     var getBooleanAttributeValue = function(attributeValues, attributeCode) {
